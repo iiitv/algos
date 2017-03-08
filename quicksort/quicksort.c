c475d65588f641b575687fc000b445c9fafa229a
Avi Aryan <avi.aryan123@gmail.com>
2017-03-09 01:42:42 +0530

    refactor: added comments

--- quicksort/quicksort.c
+++ quicksort/quicksort.c
@@ -1,39 +1,49 @@
 #include "stdio.h"
 
-
-void quicksort(int ar_size, int *  ar) {
+/*
+ * @ar_size array size
+ * @ar array pointer
+ */
+void quicksort(int ar_size, int * ar) {
 	int piv;
-	piv = ar[ar_size-1];
-	int i, temp, c=0;
-	int prevmax, prevind=-1;
+	piv = ar[ar_size-1]; // chose last element as pivot
+	int i, temp, c = 0;
+	int prevmax, prevind = -1; // stores last number >= pivot
 
 	for (i=0; i<ar_size-1; i++){
 		if (ar[i] < piv){
-			if ((prevind>-1) && (ar[i] < prevmax)){
+			if ((prevind > -1) && (ar[i] < prevmax)){
+				// swap current and previous large element
+				// current element is less than prevmax so move it to the left side
+				// and prevmax to the right side
 				temp = ar[i];
 				ar[i] = prevmax;
 				ar[prevind] = temp;
-				prevind = prevind+1;
+				// prevind now has a less than pivot element, no need for it now
+				// therefore increment it for the next number
+				prevind = prevind + 1;
 				prevmax = ar[prevind];
 			}
-			c++;
+			c++;  // pivot's final location (depends on count of items smaller than pivot)
 		}
-		else if (prevind<0) {
+		else if (prevind < 0) {
+			// init first larger than pivot element
+			// it may be swapped out later (above block)
 			prevmax = ar[i];
 			prevind = i;
 		}
 	}
 
-	if (prevind>-1){
+	if (prevind > -1){ // if some swaps were done
 		ar[ar_size-1] = ar[c];
-		ar[c] = piv;
+		ar[c] = piv; // swap pivot to its correct position
 	}
 
-	if (ar_size>2){
-		if (c!=0)
-			quicksort(c, &ar[0]);
+	if (ar_size > 2){
+		if (c != 0)
+			quicksort(c, &ar[0]); // recursively quicksort left partition
 		if (ar_size-c-1 != 0)
-			quicksort(ar_size-c-1, &ar[c+1]);
+			quicksort(ar_size-c-1, &ar[c+1]); // right partition
 	}
 }
 
