7257a551c66bb5c62f20f910a9772c9cf6574d86
Monal Shadi <monalshadi16@gmail.com>
2017-03-10 01:01:08 +0530

    Function name modified as file name

--- euclidean_gcd/euclidean_gcd.c
+++ euclidean_gcd/euclidean_gcd.c
@@ -6,14 +6,14 @@
 * There are two implementations: Recursive (gcd_recursive) and Non-Recursive (gcd_iterative)
 */
 
-int gcd_recursive(int first, int second) {
+int euclidean_gcd_recursive(int first, int second) {
         if(second == 0)
             return first;                //first becomes gcd if second becomes zero
         else
             return gcd_recursive(second, (first % second));
 }
 
-int gcd_iterative(int first, int second) {
+int euclidean_gcd(int first, int second) {
         while(second != 0) {            // Iterate till second becomes zero
             int temp = second;          // temporary variable to hold value of second which is to be assigned to first later
             second = first % second;
@@ -25,8 +25,8 @@ int gcd_iterative(int first, int second) {
 int main() {
         int first = 49;
         int second = 7;
-        int answer_recursive = gcd_recursive(first, second);
-        int answer_iterative = gcd_iterative(first, second);
+        int answer_recursive = euclidean_gcd_recursive(first, second);
+        int answer_iterative = euclidean_gcd(first, second);
         printf("GCD of %d and %d is : %d by recursive algo.\n", first, second, answer_recursive);
         printf("GCD of %d and %d is : %d by Iterative algo.\n", first, second, answer_iterative);
 }
