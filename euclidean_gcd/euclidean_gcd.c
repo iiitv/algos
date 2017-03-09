c1c58107b2ed045401f8d94da39a44c8de31ced4
Monal Shadi <monalshadi16@gmail.com>
2017-03-10 00:06:08 +0530

    Blank spaces removed and tested for both now

--- euclidean_gcd/euclidean_gcd.c
+++ euclidean_gcd/euclidean_gcd.c
@@ -3,28 +3,23 @@
 /*
 * first --> First number
 * second --> Second number
-*
 * There are two implementations: Recursive (gcd_recursive) and Non-Recursive (gcd_iterative)
-*
-*
 */
 
 int gcd_recursive(int first, int second) {
         if(second == 0)
-            return first;      			 //first becomes gcd if second becomes zero
+            return first;                //first becomes gcd if second becomes zero
         else
             return gcd_recursive(second, (first % second));
 }
 
 int gcd_iterative(int first, int second) {
-		while(second ! = 0) {			// Iterate till second becomes zero
-			int temp = second;			// temporary variable to hold value of second which is to be assigned to first later
-			second = first % second;
-			first = temp;
-		}
-
-		return first;			// When second becomes 0, first becomes gcd of both
-		
+        while(second != 0) {            // Iterate till second becomes zero
+            int temp = second;          // temporary variable to hold value of second which is to be assigned to first later
+            second = first % second;
+            first = temp;
+        }
+        return first;           // When second becomes 0, first becomes gcd of both
 }
 
 int main() {
@@ -32,5 +27,6 @@ int main() {
         int second = 7;
         int answer = gcd_recursive(first, second);
 
-        printf("GCD of %d and %d is : %d\n",first,second,answer);
+        printf("GCD of %d and %d is : %d by recursive algo.\n",first,second,answer);
+        printf("GCD of %d and %d is : %d by Iterative algo.\n",first,second,answer);
 }
