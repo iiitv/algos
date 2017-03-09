8eb9c97adc204de9e16fd94230cb5455c4b73023
Monal Shadi <monalshadi16@gmail.com>
2017-03-10 02:06:02 +0530

    Entry added in README, euclidean_gcd.py added

---
+++ euclidean_gcd/euclidean_gcd.py
@@ -0,0 +1,29 @@
+"""
+ :param first --> First number
+ :param second --> Second number
+ There are two implementations: Recursive (euclidean_gcd_recursive) and Non-Recursive (euclidean_gcd)
+"""
+
+def euclidean_gcd_recursive(first, second):
+	if second == 0:
+		return first        # First becomes gcd if second becomes zero
+	else:
+		return euclidean_gcd(second, first % second )
+
+def euclidean_gcd(first, second):
+	while second != 0:		# Iterate till second becomes zero
+		temp = second 		# Temporary variable to hold value of second which is to be assigned to first later
+		second = first % second
+		first = temp
+	return first 		# When second becomes 0, first becomes gcd of both
+
+def main():
+	first = 36
+	second = 6
+	answer_iterative = euclidean_gcd(first, second)
+	answer_recursive = euclidean_gcd_recursive(first, second)
+	print("GCD of %d and %d is : %d by recursive algo.\n" % (first, second, answer_recursive) )
+	print("GCD of %d and %d is : %d by iterative algo.\n" % (first, second, answer_iterative) )
+
+if __name__ == '__main__':
+	main()
