18be140a88b23787349556007cb0cf959e299ebd
Monal Shadi <monalshadi16@gmail.com>
2017-03-10 14:58:09 +0530

    Line spacing corrected, \n removed

--- euclidean_gcd/euclidean_gcd.py
+++ euclidean_gcd/euclidean_gcd.py
@@ -10,6 +10,7 @@ def euclidean_gcd_recursive(first, second):
 	else:
 		return euclidean_gcd(second, first % second )
 
+
 def euclidean_gcd(first, second):
 	while second != 0:		# Iterate till second becomes zero
 		temp = second 		# Temporary variable to hold value of second which is to be assigned to first later
@@ -17,13 +18,15 @@ def euclidean_gcd(first, second):
 		first = temp
 	return first 		# When second becomes 0, first becomes gcd of both
 
+
 def main():
 	first = 36
 	second = 6
 	answer_iterative = euclidean_gcd(first, second)
 	answer_recursive = euclidean_gcd_recursive(first, second)
-	print("GCD of %d and %d is : %d by recursive algo.\n" % (first, second, answer_recursive) )
-	print("GCD of %d and %d is : %d by iterative algo.\n" % (first, second, answer_iterative) )
+	print("GCD of %d and %d is : %d by recursive algo." % (first, second, answer_recursive) )
+	print("GCD of %d and %d is : %d by iterative algo." % (first, second, answer_iterative) )
+
 
 if __name__ == '__main__':
 	main()
