9a7fb12048de76a43d154a229ef6c5b9db47ed52
Monal Shadi <monalshadi16@gmail.com>
2017-03-14 17:15:03 +0530

    Extra blank line removed

--- euclidean_gcd/EuclideanGCD.java
+++ euclidean_gcd/EuclideanGCD.java
@@ -15,14 +15,12 @@ public class EuclideanGCD {
         return first;       // When second becomes 0, first becomes gcd of both
     }
 
-
     static int euclideanGCDRecursive(int first, int second) {
         if(second == 0)
             return first;       // First becomes gcd if second becomes zero
         else
             return euclideanGCDRecursive(second, (first % second));
     }
-
 
     public static void main(String args[]) {
         int first = 25;
