6f27ce4cc860eb707f45561c4648df38076b1cae
Aashutosh Rathi <aashutoshrathi@gmail.com>
2017-03-15 17:08:32 +0530

    Update sieveOfEratosthenes.c

--- sieve_of_eratosthenes/sieveOfEratosthenes.c
+++ sieve_of_eratosthenes/sieveOfEratosthenes.c
@@ -3,7 +3,7 @@
 #include <math.h>     // for using square root 
 #include <stdbool.h>  // to include bool datatype 
 
-void sieveOfEratosthenes(int n){
+void sieveOfEratosthenes(int n) {
 	bool primes[n+1];
 	int i, sqrt_n = sqrt(n)+1, j;
 	
@@ -11,22 +11,22 @@ void sieveOfEratosthenes(int n){
 	primes[0] = false;
 	primes[1] = false;
 
-	for (i = 2; i < sqrt_n; i++){
-		if (primes[i]){
-			for (j = 2*i; j < n+1; j += i){
+	for (i = 2; i < sqrt_n; i++) {
+		if (primes[i]) {
+			for (j = 2*i; j < n+1; j += i) {
 				primes[j] = false;
 			}
 		}
 	}
 
-	for (j = 2; j < n; j++){
-		if (primes[j]){
+	for (j = 2; j < n; j++) {
+		if (primes[j]) {
 			printf("%d ",j);
 		}
 	}
 }
 
-int main(){
+int main() {
 	int n;
 	scanf("%d",&n);
 	sieveOfEratosthenes(n);
