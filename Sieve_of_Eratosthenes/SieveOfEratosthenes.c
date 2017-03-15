eccf7005cc420d454b1b3b0d93e25ee53d8bbe5c
Aashutosh Rathi <aashutoshrathi@gmail.com>
2017-03-15 15:39:27 +0530

    Update SieveOfEratosthenes.c

--- Sieve_of_Eratosthenes/SieveOfEratosthenes.c
+++ Sieve_of_Eratosthenes/SieveOfEratosthenes.c
@@ -6,19 +6,17 @@ void sieveOfEratosthenes(int n){
 	bool numbers[n+1];
 	int i, sqrt_n = sqrt(n)+1, j;
 	
-	for (i = 0; i < n+1; i++){
-		numbers[i] = true;      // assigning every number "true" 
-	}
+	memset(numbers, true, sizeof(numbers));
 
 	for (i = 2; i < sqrt_n; i++){
 		if (numbers[i]){
-			for (j = 2*i; j < n+1; j = j+i){
+			for (j = 2*i; j < n+1; j += i){
 				numbers[j] = false;
 			}
 		}
 	}
 
-	for (j=2; j < n; j++){
+	for (j = 2; j < n; j++){
 		if (numbers[j]){
 			printf("%d ",j);
 		}
