1a9f54e5ef5825f334494491ab845b10a9d0d38c
jsroyal <jsroyal55@gmail.com>
2017-03-16 00:57:04 +0530

    Change accoding code of conduct

--- binary_search/BinarySearch.java
+++ binary_search/BinarySearch.java
@@ -1,33 +1,30 @@
+
 public class BinarySearch {
 
     public static void main(String[] args) {
-        int arr[] = {1, 5, 35, 112, 258, 324},
-                search_arr[] = {1, 35, 112, 324, 67},
-                pos, i;
-        BinarySearch obj = new BinarySearch();
-
-        for (i = 0; i < 5; i++) {
-            pos = obj.binarySearch(arr, 6, search_arr[i]);
-
+        int[] arr = {1, 5, 35, 112, 258, 324},
+                searchArr = {1, 35, 112, 324, 67};
+        int pos;
+        for (int i = 0; i < 5; i++) {
+            pos = binarySearch(arr, 6, searchArr[i]);
             if (pos >= 0) {
-                System.out.println(search_arr[i] + "-> found at index : " + pos);
+                System.out.println(searchArr[i] + "-> found at index : " + pos);
             } else {
-                System.out.println(search_arr[i] + " -> not found");
+                System.out.println(searchArr[i] + " -> not found");
             }
         }
 
     }
 
-    public int binarySearch(int arr[], int arr_size, int search_element) {
-        int left = 0, right = arr_size - 1;
+    static int binarySearch(int[] arr, int arrSize, int searchElement) {
+        int left = 0, right = arrSize - 1;
         while (left <= right) {
             int mid = (left + right) / 2;
-
-            if (arr[mid] == search_element) // Element found
+            if (arr[mid] == searchElement) // Element found
             {
                 return mid;
             }
-            if (arr[mid] < search_element) // Look in right half
+            if (arr[mid] < searchElement) // Look in right half
             {
                 left = mid + 1;
             } else // Look in left half
@@ -36,7 +33,7 @@ public class BinarySearch {
             }
         }
 
-        return -1;				// Element not found
+        return -1;              // Element not found
     }
 
 }
