a8ef398c908dd98e7eba226d571872715596897c
jsroyal <jsroyal55@gmail.com>
2017-03-16 13:40:09 +0530

    Changes code of conduct

--- binary_search/BinarySearch.java
+++ binary_search/BinarySearch.java
@@ -1,9 +1,26 @@
-
 public class BinarySearch {
 
+    static int binarySearch(int[] arr, int arrSize, int searchElement) {
+        int left = 0;
+        int right = arrSize - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == searchElement) { // Element found            
+                return mid;
+            }
+            if (arr[mid] < searchElement) { // Look in right half            
+                left = mid + 1;
+            } else { // Look in left half            
+                right = mid - 1;
+            }
+        }
+
+        return -1;        // Element not found
+    }
+
     public static void main(String[] args) {
-        int[] arr = {1, 5, 35, 112, 258, 324},
-                searchArr = {1, 35, 112, 324, 67};
+        int[] arr = new int[] {1, 5, 35, 112, 258, 324};
+        int[] searchArr = new int[] {1, 35, 112, 324, 67};
         int pos;
         for (int i = 0; i < 5; i++) {
             pos = binarySearch(arr, 6, searchArr[i]);
@@ -13,27 +30,5 @@ public class BinarySearch {
                 System.out.println(searchArr[i] + " -> not found");
             }
         }
-
     }
-
-    static int binarySearch(int[] arr, int arrSize, int searchElement) {
-        int left = 0, right = arrSize - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (arr[mid] == searchElement) // Element found
-            {
-                return mid;
-            }
-            if (arr[mid] < searchElement) // Look in right half
-            {
-                left = mid + 1;
-            } else // Look in left half
-            {
-                right = mid - 1;
-            }
-        }
-
-        return -1;              // Element not found
-    }
-
 }
