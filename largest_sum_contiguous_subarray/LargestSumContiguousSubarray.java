25b8bf6303b4ea83c130cef6b53c13d09733dbf6
Sandeep Kumar <sandeep201451066@users.noreply.github.com>
2017-03-22 23:59:47 +0530

    Updates Largest Sum Contiguous Subarray

--- largest_sum_contiguous_subarray/LargestSumContiguousSubarray.java
+++ largest_sum_contiguous_subarray/LargestSumContiguousSubarray.java
@@ -1,39 +1,34 @@
 /* 
    *   Implementation of famous dynamic programming problem
-   *   Largest Sum Contiguous Subarray
-   *   Kadane Algorithm 
+   *   Largest Sum Contiguous Subarray 
    *   Time complexity  O(n)
 */
 package largestsum;
 
 public class LargestSumContiguousSubarray {
 
-	private static void maxSumSubArray(int[] array) {   //  maximum sum method implemention
+	private static int largestSumContiguousSubarray(int[] array) {   //  maximum sum method implemention
 		int prevSum;
 		int currentSum;
 		int i;
 		prevSum = array[0];						// initialize current sum amd previous sum
 		currentSum = array[0];
-
-		for(i = 1; i < array.length; i++){
+		for (i = 1; i < array.length; i++) { 
 			currentSum += array[i];				// add values in current sum
-			if(currentSum < 0){					// if current sum is negative , make it zero
+			if (currentSum < 0) {					// if current sum is negative , make it zero
 				currentSum = 0;
-			}else{
+			} else {
 				if (currentSum > prevSum) {		// if current sum is greate than previous sum
 					prevSum = currentSum;			// update previous sum
 				}
 			}
 		}
-		System.out.println("Maximum Sum of Subarray:" + "\t" + prevSum);
+		return prevSum;
 	}
 
-	public static void main(String ar[]){
+	public static void main(String ar[]) {
 		
-			int[] array = {1, 3, -7, 1, 1};     
-			maxSumSubArray(array);  /// call method
-		
+		int[] array = new int[] {1, 3, -7, 1, 1};
+		System.out.println("Largest Sum of Contiguous Subarray:" + "\t" + largestSumContiguousSubarray(array));
 	}
 }
-
-
