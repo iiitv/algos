d1538f2c5112fda8e6a9b3e2d52589b1cded8c79
rajeeviiit <singhrajiv774@gmail.com>
2017-03-24 09:12:44 +0530

    indentent fixed

--- insertion_sort/insertion_sort.py
+++ insertion_sort/insertion_sort.py
@@ -1,15 +1,16 @@
 def insertionSort(alist):
-   """
-   Performs a insertion Sort
-   :param alist: Iterable of elements   
-   """
-   for index in range(1,len(alist)):
-     currentvalue = alist[index]
-     position = index    
-     while position>0 and alist[position-1]>currentvalue:
+    """
+    Performs a insertion Sort
+    :type alist: object
+    :param alist: Iterable of elements
+    """
+    for index in range(1,len(alist)):
+       currentvalue = alist[index]
+       position = index
+    while position>0 and alist[position-1]>currentvalue:
          alist[position] = alist[position-1]
-         position = position-1
-     alist[position] = currentvalue
+         position -= 1
+         alist[position] = currentvalue
 
 def main():
     alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
