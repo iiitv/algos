f3954fa3849f2028d1b7abb02d81d8c8ead3ea5b
rajeeviiit <singhrajiv774@gmail.com>
2017-03-24 21:22:31 +0530

    re indent

--- insertion_sort/insertion_sort.py
+++ insertion_sort/insertion_sort.py
@@ -7,9 +7,9 @@ def insertionSort(alist):
         current_value = alist[index]
         position = index
     while position > 0 and alist[position-1] > current_value:
-         alist[position] = alist[position-1]
-         position -= 1
-         alist[position] = current_value
+        alist[position] = alist[position-1]
+        position -= 1
+        alist[position] = current_value
 
 
 def main():
