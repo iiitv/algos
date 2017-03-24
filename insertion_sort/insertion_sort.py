43cc8d045d8aa0525ddb5de7876a72b93ed46c57
rajeeviiit <singhrajiv774@gmail.com>
2017-03-24 20:39:22 +0530

    updated

--- insertion_sort/insertion_sort.py
+++ insertion_sort/insertion_sort.py
@@ -3,13 +3,13 @@ def insertionSort(alist):
     Performs a insertion sort
     :type alist: object
     """
-    for index in range(1,len(alist)):
-        currentvalue = alist[index]
+    for index in range(1, len(alist)):
+        current_value = alist[index]
         position = index
-    while position>0 and alist[position-1]>currentvalue:
+    while position > 0 and alist[position-1] > current_value:
          alist[position] = alist[position-1]
          position -= 1
-         alist[position] = currentvalue
+         alist[position] = current_value
 
 
 def main():
