f57a2d8cee887d9f6891047cd4828fe8c78f1b15
Aashutosh Rathi <aashutoshrathi@gmail.com>
2017-03-25 10:31:12 +0530

    Added Binary Search [Python]

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -1,7 +1,7 @@
 from random import randint
 
 
-def binarySearchIterative(array, left, right, element):
+def binary_search_iterative(array, left, right, element):
     while left <= right:
         mid = int(left + (right - left) / 2)
         # indices of a list must be integer
@@ -14,15 +14,15 @@ def binarySearchIterative(array, left, right, element):
     return -1
 
 
-def binarySearchRecursive(array, left, right, element):
+def binary_search_recursive(array, left, right, element):
     if right >= left:
         mid = int(left + (right - left) / 2)
         if array[mid] == element:
             return mid
         elif array[mid] > element:
-            return binarySearchRecursive(array, left, mid - 1, element)
+            return binary_search_recursive(array, left, mid - 1, element)
         else:
-            return binarySearchRecursive(array, mid + 1, right, element)
+            return binary_search_recursive(array, mid + 1, right, element)
     else:
         return -1
 
@@ -38,12 +38,12 @@ def main():
     # i.e. a random element can be selected from domain
     print('Element = ', element)
     length = len(array)
-    result = binarySearchRecursive(array, 0, length - 1, element)
+    result = binary_search_recursive(array, 0, length - 1, element)
     if result != -1:
         print('Recursive Binary Search : Element is present at index', result)
     else:
         print('Recursive Binary Search : Element is not present in array')
-    result = binarySearchIterative(array, 0, length - 1, element)
+    result = binary_search_iterative(array, 0, length - 1, element)
     if result != -1:
         print('Iterative Binary Search : Element is present at index', result)
     else:
