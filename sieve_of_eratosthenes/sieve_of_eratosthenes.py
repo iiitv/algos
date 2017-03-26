d4dfa469007eec7888344dae3d80e45f6deaeca9
Monal Shadi <monalshadi16@gmail.com>
2017-03-26 15:29:45 +0530

    Minor fixes

--- sieve_of_eratosthenes/sieve_of_eratosthenes.py
+++ sieve_of_eratosthenes/sieve_of_eratosthenes.py
@@ -11,11 +11,10 @@ def sieve_of_eratosthenes(n):
     primes = [True for i in range(n+1)]    # Setting all as True initially
     primes[0] = primes[1] = False    # Since 0 and 1 are not primes
     sqrt_n = int(math.sqrt(n)) + 1
-    for i in range(2, sqrt_n+1):
+    for i in range(2, sqrt_n+1, 1):
         if primes[i]:
             for j in range(2*i, n+1, i):
                 primes[j] = False
-        i += 1
     return primes
 
 
