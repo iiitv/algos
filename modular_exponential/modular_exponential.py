8370193ea18af43a338002fb4fa8953c7338bee1
yashLadha <201551061@iiitvadodara.ac.in>
2017-03-26 09:06:49 +0530

    Changed Arguments name

--- modular_exponential/modular_exponential.py
+++ modular_exponential/modular_exponential.py
@@ -1,16 +1,17 @@
-def mod_exponent(_base, _pow, _mod):
+def mod_exponent(base_, pow_, mod_):
     res = 1  # Initialize result
-    _base = _base % _mod  # Update base if it is more than or equal _mod
-    while _pow > 0:
-        if _pow & 1:  # if _pow is odd multiply it with result
-            res = (res * _base) % _mod
-        _pow = _pow >> 1  # _pow must be even now
-        _base = (_base * _base) % _mod
+    base_ = base_ % mod_  # Update base if it is more than or equal mod_
+    while pow_ > 0:
+        if pow_ & 1:  # if pow_ is odd multiply it with result
+            res = (res * base_) % mod_
+        pow_ = pow_ >> 1  # _pow must be even now
+        base_ = (base_ * base_) % mod_
     return res
 
 
 if __name__ == '__main__':
-    base = 2
-    power = 5
-    mod = 6
+    base = int(input())
+    power = int(input())
+    mod = int(input())
     print(mod_exponent(base, power, mod))
+
