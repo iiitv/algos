6502d5faa4160f1953f125a1b827235a70554787
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2017-03-27 19:18:11 +0530

    Update LargestSumContiguousSubarray.cpp

--- largest_sum_contiguous_subarray/LargestSumContiguousSubarray.cpp
+++ largest_sum_contiguous_subarray/LargestSumContiguousSubarray.cpp
@@ -14,12 +14,12 @@ int largestSumContiguousSubarray(int length, int arr[]) {
     prevSum = arr[0];						    // initialize current sum and previous sum
     currentSum = arr[0];
     for (i = 1; i < length; i++) {
-        currentSum += arr[i];				    // add values in current sum
-        if (currentSum < 0) {					// if current sum is negative , make it zero
+        currentSum += arr[i];				        // add values in current sum
+        if (currentSum < 0) {					   // if current sum is negative , make it zero
             currentSum = 0;
         }
-        else if (currentSum > prevSum) {        // if current sum is great than previous sum
-            prevSum = currentSum;               // update previous sum
+        else if (currentSum > prevSum) {           // if current sum is great than previous sum
+            prevSum = currentSum;                  // update previous sum
         }
     }
     return prevSum;
@@ -27,7 +27,7 @@ int largestSumContiguousSubarray(int length, int arr[]) {
 
 int main() {
     int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-    int length = sizeof(arr)/sizeof(arr[0]);    // Length(Size) of the array
+    int length = sizeof(arr)/sizeof(arr[0]);         // Length(Size) of the array
     cout << "Largest Sum of Contiguous Subarray = " << largestSumContiguousSubarray(length, arr) << "\n";
     return 0;
 }
