9739cdd545db66f4b7960f3d6c4c5f4107378ab0
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2017-03-27 13:53:17 +0530

    Add files via upload

---
+++ largest_sum_contiguous_subarray/LargestSumContiguousSubarray.cpp
@@ -0,0 +1,32 @@
+/*
+ *   Implementation of famous dynamic programming problem
+ *   Largest Sum Contiguous Subarray
+ *   Kadane Algorithm
+ *   Time complexity  O(n)
+ */
+#include <iostream>
+using namespace std;
+
+int largestSumContiguousSubarray(int arr[]) {
+    int prevSum;
+    int currentSum;
+    int i;
+    prevSum = arr[0];						    // initialize current sum amd previous sum
+    currentSum = arr[0];
+    for (i = 1; i < length; i++) {
+        currentSum += arr[i];				    // add values in current sum
+        if (currentSum < 0) {					// if current sum is negative , make it zero
+            currentSum = 0;
+        }
+        else if (currentSum > prevSum) {        // if current sum is greate than previous sum
+            prevSum = currentSum;               // update previous sum
+        }
+    }
+    return prevSum;
+}
+
+int main() {
+    int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    cout << "Largest Sum of Contiguous Subarray = " << largestSumContiguousSubarray(arr) << "\n";
+    return 0;
+}
