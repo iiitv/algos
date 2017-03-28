49a0c1014b3c23ffa7ff3a826e4598170bf1f55c
Lucifer24 <divyeshpuri66@gmail.com>
2017-03-29 00:11:59 +0530

    Added  Largest  Sum Continous SubArray [C]

---
+++ largest_sum_contiguous_subarray/LargestSumContiguousSubarray.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int LargestSumContinousSubArray(int a[], int size) {
+	int max_till = 0;
+	int max_int = 0;
+	for (int i = 0; i < size; i++) {
+		max_int = max_int +a[i];
+		if(max_till < max_int) {
+			max_till = max_int;
+		}
+		if (max_int < 0) {
+			max_int = 0;
+		}
+	}
+	return max_till;
+}
+
+int main() {
+	int array [10];
+	for (int k = 0; k < 10; k++)
+	{
+		array[k] = rand()%10;
+	}
+	printf("%d", LargestSumContinousSubArray(array, 10));
+}
