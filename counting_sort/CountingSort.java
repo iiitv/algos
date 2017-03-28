2c1ec7e0cdda21ce57b8dd6a8f719310741d2e42
Hsankesara <anshu.av97@gmail.com>
2017-03-29 01:47:55 +0530

    Changes according to @Monal5031

--- counting_sort/CountingSort.java
+++ counting_sort/CountingSort.java
@@ -1,9 +1,7 @@
 import java.util.Random;
  class CountingSort {
-	
 	public static void main(String[] args) {
-		
-		int A[]=new int[10000];
+		int[] A=new int[10000];
         int k=0;
         Random rand=new Random();
         for(int i=0;i<A.length;i++) {
@@ -11,21 +9,18 @@ import java.util.Random;
             if(k<A[i])
             	k=A[i];
             }
-        
-        A=cSort(A,k);
+        A=cSort(A, k);
         for(int i=0;i<A.length;i++)
         	System.out.print(A[i] + " ");
 	}
-
 	public static int[] cSort(int[] a,int k) {
 		int[] b=new int[a.length];
 		int[] c= new int[k+1];
 		 getC(a,c);
-        for(int i=a.length-1;i>=0;i--){
+        for (int i=a.length-1;i >=0;i--) {
             b[c[a[i]]-1]=a[i];
             c[a[i]]--;
-        }
-            
+        }       
         return b;	
 	}
 	private static void getC(int[] a, int[] c) {
@@ -34,5 +29,4 @@ import java.util.Random;
        for(int i=1;i<c.length;i++)
            c[i]=c[i-1]+c[i];
     }
-
 }
