66b4951f25c4a271c33fbad537372ca767e8f542
jsroyal <jsroyal55@gmail.com>
2017-03-28 23:48:03 +0530

    implement using stack

--- depth_first_traversal/DepthFirstTraversal.java
+++ depth_first_traversal/DepthFirstTraversal.java
@@ -1,21 +1,46 @@
+import java.util.Stack;
+
+//implemented Depth First Travesal in given "Directed graph" (Adjacany matrix)
 class DepthFirstTraversal {
 
-    public static int[] arr;    // DFS traversal path
+    public static int[] arr;
     public static int k = 0;
-    
-    public static void depthFirstTraversal(int a[][], int visited[], int i, int n) {
-        arr[k] = i + 1;
+
+    public static void depthFirstTraversal(int a[][], int[] visited,int source){
+        for (int i = 0; i < visited.length; i++) {
+            visited[i] = -1;
+        }
+        Stack<Integer>stack = new Stack();
+        int element = source;       
+        int i = source;
+        int n = visited.length - 1;
+        arr[k] = element;
         k++;
-        visited[i] = 1;    // mark as visit 
-        for (int j = 0; j < n; j++) {
-            if (a[i][j] == 1 && visited[j] == 0) {    //check connected and visited
-                depthFirstTraversal(a, visited, j, n);    //recursive call
+        visited[source] = 1;        
+        stack.push(source);
+ 
+        while (!stack.isEmpty()) {    
+            element = stack.peek();
+            i = element;    
+            while (i <= n) {
+                if (a[element][i] == 1 && visited[i] == -1) {
+                    stack.push(i);
+                    visited[i] = 1;
+                    element = i;
+                    i = 1;
+                    arr[k] = element;
+                    k++;                
+                    continue;
+                }
+                i++;
             }
+            stack.pop();    
         }
     }
+    
 
     public static void main(String args[]) {
-        int n = 5;    // No. of vertices
+        int n = 5;    //no. of vertic
         int[] visited = new int[n]; 
         arr = new int[n];
         // Adjacency Matrix of graph
@@ -24,16 +49,12 @@ class DepthFirstTraversal {
                                  {1, 1, 0, 1, 0},
                                  {0, 0, 0, 0, 0},
                                  {0, 0, 1, 0, 0}};
-        for (int i = 0; i < n; i++) {
-            visited[i] = 0;
-        }
-        for (int i = 0; i < n; i++) {
-            if (visited[i] == 0) {
-                depthFirstTraversal(a, visited, i, n);
-            }
-        }
+        depthFirstTraversal(a, visited, 0);
+        
         for (int element : arr) {
+            if(element != -1)   //not direct connected 
             System.out.println(element);
         }
     }
 }
+
