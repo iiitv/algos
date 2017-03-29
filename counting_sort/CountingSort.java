d961c93362fbb3401741e35be95105351d66cb2d
Heet Sankesara <heetsankesara3@gmail.com>
2017-03-29 18:47:42 +0530

    Use Bottom down approach

--- counting_sort/CountingSort.java
+++ counting_sort/CountingSort.java
@@ -1,19 +1,11 @@
 import java.util.Random;
  class CountingSort {
-	public static void main(String[] args) {
-		int[] A = new int[10000];
-        	int k = 0;
-        	Random rand=new Random();
-        	for (int i = 0;i < A.length;i++) {
-            		A[i] = rand.nextInt(100);
-            		if(k < A[i])
-            			k = A[i];
-            	}
-       		 A = countingSort(A, k);
-        	for (int i = 0;i < A.length;i++)
-        		System.out.print(A[i] + " ");
-	}
-	 
+	private static void countOccurences(int[] a, int[] c) {
+       		for(int i = 0;i < a.length;i++)
+           		c[a[i]]++;
+       		for(int i = 1;i < c.length;i++)
+           		c[i] = c[i-1] + c[i];
+    	}
 	public static int[] countingSort(int[] a, int k) {
 		int[] b = new int[a.length];
 		int[] c = new int[k+1];
@@ -22,13 +14,19 @@ import java.util.Random;
             		b[c[a[i]]-1] = a[i];
             		c[a[i]]--;
         	}       
-        return b;	
+        	return b;	
 	}
-	 
-	private static void countOccurences(int[] a, int[] c) {
-       		for(int i = 0;i < a.length;i++)
-           		c[a[i]]++;
-       		for(int i = 1;i < c.length;i++)
-           		c[i] = c[i-1] + c[i];
-    	}
+	 public static void main(String[] args) {
+		int[] A = new int[10000];
+        	int k = 0;
+        	Random rand=new Random();
+        	for (int i = 0;i < A.length;i++) {
+            		A[i] = rand.nextInt(100);
+            		if(k < A[i])
+            			k = A[i];
+          	}
+       		 A = countingSort(A, k);
+        	for (int i = 0;i < A.length;i++)
+        		System.out.print(A[i] + " ");
+	}
 }
