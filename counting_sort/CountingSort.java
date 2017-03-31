3cc1261fb52564f53508136dbed60f0acb326c97
Heet Sankesara <heetsankesara3@gmail.com>
2017-03-31 13:23:43 +0530

    Add Comments in Code

--- counting_sort/CountingSort.java
+++ counting_sort/CountingSort.java
@@ -1,20 +1,20 @@
 import java.util.Random;
-class CountingSort {
-	private static void countOccurences(int[] a, int[] c) {
-		for (int i = 0; i < a.length; i++)
+class CountingSort {	//Time complexity = O(n) , but every number must be between 0 to k
+	private static void countOccurences(int[] a, int[] c) {	//Basically counting occurences of particular  number and store in its index
+		for (int i = 0; i < a.length; i++)	//Counting occurence of each number
            		c[a[i]]++;
-		for(int i = 1;i < c.length;i++)
+		for(int i = 1;i < c.length;i++)		//counting total number of numbers less then or equal to that number
            		c[i] = c[i-1] + c[i];
     	}
-	public static int[] countingSort(int[] a, int k) {
-		int[] b = new int[a.length];
-		int[] c = new int[k+1];
+	public static int[] countingSort(int[] a, int k) { // For Sorting
+		int[] b = new int[a.length];	//b Array stores  sorted array
+		int[] c = new int[k+1];		//array  c count occurences
 		countOccurences(a, c);
-        	for (int i = a.length-1;i >= 0;i--) {
-            		b[c[a[i]]-1] = a[i];
-            		c[a[i]]--;
+        	for (int i = a.length-1;i >= 0;i--) {	//storing number in ascending order in b
+            		b[c[a[i]]-1] = a[i];	//Stores number to it respective position	 
+            		c[a[i]]--;		//Decrease c 
         	}       
-        	return b;	
+        	return b;	//Return sorted array
 	}
 	public static void main(String[] args) {
 		int[] A = new int[10000];
