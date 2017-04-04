61be9730344cf5f77a310664d466927cca55e378
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2017-04-04 08:44:05 +0530

    Changes done.
    Added an extra blank line before "return 0".
    Removed extra whitespace from line 53
    Removed hard coded 7 from line 55

--- heap_sort/HeapSort.c
+++ heap_sort/HeapSort.c
@@ -7,54 +7,56 @@
 #include<stdio.h>
 #include <stdlib.h>
 
-void  max_heapify(int a[], int i, int heapsize) {
+//i is the root of subtree, a is an array, HEAPSIZE is size of heap
+void  max_heapify(int a[], int i, int HEAPSIZE) {
     int largest;
     int l = (2 * i) + 1;  // left child
     int r = (2 * i) + 2;  // Right child
     // Check if left child is larger than root.
-    if ((l <= heapsize) && (a[l] > a[i]))
+    if ((l <= HEAPSIZE) && (a[l] > a[i]))
          largest = l;
-
     else
          largest = i;
     // Check if right child is larger than root.
-    if ((r <= heapsize) && (a[r] > a[largest]))
+    if ((r <= HEAPSIZE) && (a[r] > a[largest]))
          largest = r ;
     // If root is not the largest.
     if (largest != i) {
          int tmp = a[i];
          a[i] = a[largest];
          a[largest] = tmp;
-         max_heapify(a, largest, heapsize);
+         max_heapify(a, largest, HEAPSIZE);
     }
 }
 
-void heap_sort(int a[], int heapsize) {
+// a is the array.
+void heap_sort(int a[], int HEAPSIZE) {
     int i;
     // Building max heap.
-    for (i = heapsize/2; i >= 0; i--) {
-         max_heapify(a, i, heapsize);
+    for (i = HEAPSIZE/2; i >= 0; i--) {
+         max_heapify(a, i, HEAPSIZE);
     }
     // One by one extract an element from heap
-    for (i = heapsize; i > 0; i--) {
+    for (i = HEAPSIZE; i > 0; i--) {
         int tmp = a[i];
         a[i] = a[0];
         a[0] = tmp;
-        heapsize--;
+        HEAPSIZE--;
         // Again build max heap with the reduced array.
-        max_heapify(a, 0, heapsize);
+        max_heapify(a, 0, HEAPSIZE);
     }
 }
 
 int main() {
-    int i, r;
+    int i, r, SIZE = 8;
     // Unsorted data
-    int a[8] = { 10000, -999, 240, 1111111, 3, 2, 452, -65 };
+    int a[] = {10000, -999, 240, 1111111, 3, 2, 452, -65};
     // Calling heap_sort function
-    heap_sort(a, 7);
+    heap_sort(a, SIZE-1);
     printf("After Sorting:\t");
 
-    for (i = 0; i < 8; i++)
+    for (i = 0; i < SIZE; i++)
         printf("%d ", a[i]);
+
     return 0;
 }
