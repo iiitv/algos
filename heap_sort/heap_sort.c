e627d7181affc4d84319c80fb5572300613ab756
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2017-04-07 01:12:48 +0530

    Changes requested by @Monal5031

--- heap_sort/heap_sort.c
+++ heap_sort/heap_sort.c
@@ -5,20 +5,17 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
 
-// root = index root of the subtree, a is an array, heapsize is size of heap
+// root = index root of the subtree, a is an array, heapsize = size of heap
 void  max_heapify(int a[], int root, int heapsize) {
-    int largest;
+    int largest = root;
     int l = (2 * root) + 1;  // left child
     int r = (2 * root) + 2;  // Right child
     // Check if left child is larger than root.
-    if ((l <= heapsize) && (a[l] > a[root]))
+    if ((l < heapsize) && (a[l] > a[root]))
          largest = l;
-    else
-         largest = root;
     // Check if right child is larger than largest.
-    if ((r <= heapsize) && (a[r] > a[largest]))
+    if ((r < heapsize) && (a[r] > a[largest]))
          largest = r ;
     // If root is not the largest.
     if (largest != root) {
@@ -33,11 +30,11 @@ void  max_heapify(int a[], int root, int heapsize) {
 void heap_sort(int a[], int heapsize) {
     int i;
     // Building max heap.
-    for (i = heapsize/2; i >= 0; i--) {
+    for (i = (heapsize / 2) - 1; i >= 0; i--) {
          max_heapify(a, i, heapsize);
     }
     // One by one extract an element from heap
-    for (i = heapsize; i > 0; i--) {
+    for (i = heapsize - 1; i > 0; i--) {
         int tmp = a[i];
         a[i] = a[0];
         a[0] = tmp;
@@ -51,9 +48,9 @@ int main() {
     int i, r;
     // Unsorted data
     int a[] = {10000, -999, 240, 1111111, 3, 2, 452, -65};
-    int size = sizeof(a)/sizeof(a[0]);
+    int size = sizeof(a) / sizeof(a[0]);
     // Calling heap_sort function
-    heap_sort(a, size-1);
+    heap_sort(a, size);
     printf("After Sorting:\t");
 
     for (i = 0; i < size; i++)
