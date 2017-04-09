c0a2b9617249a8b69f4da8c29eaecb151e2d884c
jsroyal <jsroyal55@gmail.com>
2017-04-09 21:54:01 +0530

    Using list and set

--- depth_first_traversal/DepthFirstTraversal.java
+++ depth_first_traversal/DepthFirstTraversal.java
@@ -1,59 +1,65 @@
-import java.util.Stack;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Set;
 
-//implemented Depth First Travesal in given "Directed graph" (Adjacany matrix)
-class DepthFirstTraversal {
+public class DepthFirstTraversal {
 
     public static int[] arr;
     public static int k = 0;
-
-    public static void depthFirstTraversal(int[][] a, int[] visited, int source){
-        for (int i = 0; i < visited.length; i++) {
-            visited[i] = -1;
-        }
-        Stack<Integer>stack = new Stack();
-        int element = source;       
-        int i = source;
-        int n = visited.length - 1;
-        arr[k] = element;
-        k++;
-        visited[source] = 1;        
-        stack.push(source);
- 
-        while (!stack.isEmpty()) {    
-            element = stack.peek();
-            i = element;    
-            while (i <= n) {
-                if (a[element][i] == 1 && visited[i] == -1) {
-                    stack.push(i);
-                    visited[i] = 1;
-                    element = i;
-                    i = 1;
-                    arr[k] = element;
-                    k++;                
-                    continue;
-                }
-                i++;
-            }
-            stack.pop();    
+    private static int V;   // No. of vertices 
+    // Array  of lists for Adjacency List Representation
+    private static LinkedList<Integer>[] adj;
+    
+    DepthFirstTraversal(int v) {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; ++i) {
+            adj[i] = new LinkedList();
         }
     }
+
+    //Function to add an edge into the DepthFirstTraversal
+    private static void addEdge(int v, int w) {
+        adj[v].add(w);  
+    }
     
+    // A function used by DFS
+    private static void depthFirstTraversal(int v, Set visited) {
+        // Mark the current node as visited 
+        visited.add(v);
+        arr[k] = v;
+        k++;        
+        Iterator<Integer> i = adj[v].listIterator();
+        while (i.hasNext()) {
+            int n = i.next();
+            if (!visited.contains(n)) {
+                depthFirstTraversal(n, visited);
+            }
+        }
+    }
+
+    public static void DFS(int v) {        
+        // false by default in java)
+        Set<Integer> visited = new HashSet();
+        // Call the recursive helper function to print DFS traversal
+        depthFirstTraversal(v, visited);
+    }
 
     public static void main(String[] args) {
-        int n = 5;    //no. of vertic
-        int[] visited = new int[n]; 
-        arr = new int[n];
-        // Adjacency Matrix of graph
-        int[][] a = new int[][] {{0, 1, 0, 1, 0},
-                                 {0, 0, 0, 1, 1},
-                                 {1, 1, 0, 1, 0},
-                                 {0, 0, 0, 0, 0},
-                                 {0, 0, 1, 0, 0}};
-        depthFirstTraversal(a, visited, 0);        
-        for (int element : arr) {
-            if(element != -1)   //not direct connected 
-            System.out.println(element);
+        DepthFirstTraversal g = new DepthFirstTraversal(4);
+        arr = new int[4];
+        addEdge(0, 1);
+        addEdge(0, 2);
+        addEdge(1, 2);
+        addEdge(2, 0);
+        addEdge(2, 3);
+        addEdge(3, 0);
+        System.out.println("Depth First Traversal starting from vertex 2");
+        DFS(3);
+
+        for (int element : arr) {            
+            System.out.println(element);            
         }
     }
 }
-
