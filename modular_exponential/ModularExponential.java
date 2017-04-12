ebb8347827ae3da7b0e8e1db5f6db8a15e018949
Aashutosh Rathi <aashutoshrathi@gmail.com>
2017-04-12 13:00:24 +0530

    minor changes

--- modular_exponential/ModularExponential.java
+++ modular_exponential/ModularExponential.java
@@ -1,10 +1,14 @@
-class ModularExponential {
-	static int modularExponential(int base, int power, int mod) {
-		int result = 1;
+import java.math.BigInteger;
+
+public class ModularExponential {
+	public static BigInteger modularExponential(long base, long power, long mod) {
+		BigInteger result = BigInteger.ONE;
 		base = base % mod;
 		while(power > 0) {
-			if(power%2==1) // i.e. if power is odd it will return 1 (true), else flase
-				result = (result * base) % mod;	
+			if(power % 2 == 1) { // i.e. if power is odd it will return 1 (true), else flase
+				result = result.multiply(BigInteger.valueOf(base));
+				result = result.mod(BigInteger.valueOf(mod));
+			}
 			power = power / 2;
 			base = (base * base) % mod;			
 		}
@@ -12,9 +16,9 @@ class ModularExponential {
 	}
 
 	public static void main(String[] args) {
-		int base = 78;
-		int pow = 20;
-		int mod = 65;
+		long base = 78;
+		long pow = 20;
+		long mod = 65;
 		System.out.println(modularExponential(base, pow, mod));
 	}
 }
