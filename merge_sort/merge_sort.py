0eb322bdfaa38f18e17a78071da1a57f2275fa25
Aashutosh Rathi <aashutoshrathi@gmail.com>
2017-04-17 01:09:34 +0530

    Changed Merge Sort [Python]

--- merge_sort/merge_sort.py
+++ merge_sort/merge_sort.py
@@ -8,40 +8,25 @@ def merge(array, left, right):
     :param right: right limit for merge sort
     :return: no returns, merges arrays.
     """
-    mid = int((left + right) / 2)
-    s1 = int(mid - left + 1)
-    s2 = int(right - mid)
-    l = [0] * s1
-    r = [0] * s2
-    for i in range(s1):
-        l[i] = array[int(left + i)]
-
-    for j in range(s2):
-        r[j] = array[mid + 1 + j]
-
-    i = j = int(0)
-    k = int(left)
-    while(i < s1 and j < s2):
-        if(l[i] <= r[j]):
-            array[k] = l[i]
-            i = i + 1
+    mid = (left + right) // 2
+    l = array[left:mid + 1]
+    r = array[mid + 1:right + 1]
+    k = left
+    while l and r:
+        if l[0] < r[0]:
+            array[k] = l.pop(0)
         else:
-            array[k] = r[j]
-            j = j + 1
-
-        k = k + 1
-
-    while(i < s1):
-        array[k] = l[i]
-        k = k + 1
-        i = i + 1
-
-    while(j < s2):
-        array[k] = r[j]
-        k = k + 1
-        j = j + 1
-
-
+            array[k] = r.pop(0)
+        k += 1
+ 
+    while l:
+        array[k] = l.pop(0)
+        k += 1
+    while r:
+        array[k] = r.pop(0)
+        k += 1
+ 
+ 
 def merge_sort(array, left, right):
     """
     Perform sort using merge function.
@@ -52,17 +37,19 @@ def merge_sort(array, left, right):
     :param right: right limit for merge sort
     :return: no returns, sorts array
     """
-    if(left < right):
-        mid = int((left + right) / 2)
+    if left < right:
+        mid = (left + right) // 2
         merge_sort(array, left, mid)
         merge_sort(array, mid + 1, right)
         merge(array, left, right)
-
-
+ 
+ 
 def main():
-    a = [15, 19, 18, 26, 456, 87, 45]
+    a = [15, 19, 18, 26, 456, 87, 45, -1, 558897984]
     merge_sort(a, 0, len(a) - 1)
     print(a)
-
+ 
+ 
 if __name__ == '__main__':
     main()
+ 
