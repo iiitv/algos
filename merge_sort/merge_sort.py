1bfc6360a85753ba54af84f879b3319da3763094
Aashutosh Rathi <aashutoshrathi@gmail.com>
2017-04-16 22:33:11 +0530

    Added Merge Sort [Python] (#139), fixes #137
    * Added Merge Sort [Python]
    
    * Added Merge Sort [Python]

---
+++ merge_sort/merge_sort.py
@@ -0,0 +1,68 @@
+def merge(array, left, right):
+    """
+    Perform Merge Operation between arrays.
+    Time Complexity: Theta(nLogn)
+    Auxiliary Space: O(n)
+    :param array: Iterable of elements
+    :param left: left limit for merge sort
+    :param right: right limit for merge sort
+    :return: no returns, merges arrays.
+    """
+    mid = int((left + right) / 2)
+    s1 = int(mid - left + 1)
+    s2 = int(right - mid)
+    l = [0] * s1
+    r = [0] * s2
+    for i in range(s1):
+        l[i] = array[int(left + i)]
+
+    for j in range(s2):
+        r[j] = array[mid + 1 + j]
+
+    i = j = int(0)
+    k = int(left)
+    while(i < s1 and j < s2):
+        if(l[i] <= r[j]):
+            array[k] = l[i]
+            i = i + 1
+        else:
+            array[k] = r[j]
+            j = j + 1
+
+        k = k + 1
+
+    while(i < s1):
+        array[k] = l[i]
+        k = k + 1
+        i = i + 1
+
+    while(j < s2):
+        array[k] = r[j]
+        k = k + 1
+        j = j + 1
+
+
+def merge_sort(array, left, right):
+    """
+    Perform sort using merge function.
+    Time Complexity : O(nlog(n)).
+    Space Complexity : O(n).
+    :param array: Iterable of elements
+    :param left: left limit for merge sort
+    :param right: right limit for merge sort
+    :return: no returns, sorts array
+    """
+    if(left < right):
+        mid = int((left + right) / 2)
+        merge_sort(array, left, mid)
+        merge_sort(array, mid + 1, right)
+        merge(array, left, right)
+
+
+def main():
+    a = [15, 19, 18, 26, 456, 87, 45]
+    merge_sort(a, 0, len(a) - 1)
+    print(a)
+
+if __name__ == '__main__':
+    main()
