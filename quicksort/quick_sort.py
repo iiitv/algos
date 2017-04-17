638f4c462d5ff82333a94a581068545840eb7775
Aashutosh Rathi <aashutoshrathi@gmail.com>
2017-04-18 03:07:58 +0530

    Quick Sort [Python] Randomize Pivot
    changed p to l
    
    Documentation of functions changed

--- quicksort/quick_sort.py
+++ quicksort/quick_sort.py
@@ -1,21 +1,37 @@
-def partition(array, p, r):
+def median(a, i, j, k):
+    """
+    Returns Median of 3 integers from array a.
+    :param a: Iterable of elements
+    :param i: index of element of set, whose meadian is to be returned
+    :param j: index of element of set, whose meadian is to be returned
+    :param k: index element of set, whose meadian is to be returned
+    :return: returns the value of index median of a[i], a[j], a[k].
+    """
+    if a[i] > a[j]:
+        return j if a[j] < a[k] else k
+    else:
+        return i if a[i] < a[k] else k
+
+
+def partition(array, l, r):
     """
     Perform Partition Operation on array.
     Time Complexity: Theta(nLogn)
     Auxiliary Space: O(n)
     :param array: Iterable of elements
-    :param p: pivot value for array
+    :param l: left limit of array
     :param r: right limit of array
-    :return: return q value for function, used in partitioning of array.
+    :return: return q (partition index) value for function, used in partitioning of array.
     """
-    i = p - 1
-    pivot = array[r]
-    for j in range(p, r):
+    i = l - 1
+    pivot_index = median(array, l, r, (l + r) // 2)
+    pivot = array[pivot_index]
+    for j in range(l, r+1):
         if array[j] <= pivot:
             i += 1
             array[i], array[j] = array[j], array[i]
     i += 1
-    array[r], array[i] = array[i], array[r]
+    array[i], array[r] = array[r], array[i]
     return i
 
 
