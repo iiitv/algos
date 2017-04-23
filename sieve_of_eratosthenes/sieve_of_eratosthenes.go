15172ca317fdb30ec8ad8d5fcec92cd31e0f4b3d
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2017-04-23 08:47:04 +0530

    Checked with GoSublime
    Corrected some linting errors

--- sieve_of_eratosthenes/sieve_of_eratosthenes.go
+++ sieve_of_eratosthenes/sieve_of_eratosthenes.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-        "fmt"
-        "math"
+	"fmt"
+	"math"
 )
 
 // N = Limit, change N to get all primes <= N
 const N = 20
 
 // SieveOfEratosthenes returns bool array primes
-func SieveOfEratosthenes()[N + 1] bool {
-    var primes[N + 1] bool
-    sqrtOfn := int(math.Floor(math.Sqrt(N)) + 1)
-    for j := range primes {
-        primes[j] = true
-    }
-    for i := 2; i <= sqrtOfn; i++ {
+func SieveOfEratosthenes() [N + 1]bool {
+	var primes [N + 1]bool
+	sqrtOfn := int(math.Floor(math.Sqrt(N)) + 1)
+	for j := range primes {
+		primes[j] = true
+	}
+	for i := 2; i <= sqrtOfn; i++ {
 		if primes[i] {
-			for j := 2*i; j < N+1; j += i {
-				primes[j] = false;
+			for j := 2 * i; j < N+1; j += i {
+				primes[j] = false
 			}
 		}
 	}
-    return primes
+	return primes
 }
 
 func main() {
-    var primes[N + 1] bool
-    primes = SieveOfEratosthenes()
-    for i := 2; i <= N; i++ {
-        if primes[i] {
-            fmt.Println(i)
-        }
-    }
+	var primes [N + 1]bool
+	primes = SieveOfEratosthenes()
+	for i := 2; i <= N; i++ {
+		if primes[i] {
+			fmt.Println(i)
+		}
+	}
 }
