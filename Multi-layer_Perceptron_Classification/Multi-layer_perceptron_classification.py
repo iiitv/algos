6d8df8acd7a7e47a26b3f6f906b378f7f686a3b6
Ravi Kishan Jha <ravikishan77@gmail.com>
2017-05-05 14:47:13 +0530

    Some more updates

--- Multi-layer_Perceptron_Classification/Multi-layer_perceptron_classification.py
+++ Multi-layer_Perceptron_Classification/Multi-layer_perceptron_classification.py
@@ -22,21 +22,21 @@ def main():
 	training_epochs = 15 #no of loops the training step runs
 	batch_size = 100 # Size of code chunks used for training prpose
 	display_step = 1 #Just a notion used to display each and every training steps details. 
-	n_hidden_1 = 256 # 1st layer number of features
-	n_input = 784 # MNIST data input (img shape: 28*28)
+	nh1 = 256 # 1st layer number of features
+	n_in = 784 # MNIST data input (img shape: 28*28)
 	n_classes = 10 # MNIST total classes (0-9 digits)
 
 	# Tensorflow Graph input of placeholder type
-	x = tf.placeholder("float", [None, n_input])
+	x = tf.placeholder("float", [None, n_in])
 	y = tf.placeholder("float", [None, n_classes])
 
 	# Weights and Biases are defined over here.
 	weights = {
-	    'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
-	    'out': tf.Variable(tf.random_normal([n_hidden_1, n_classes]))
+	    'h1': tf.Variable(tf.random_normal([n_in, nh1])),
+	    'out': tf.Variable(tf.random_normal([nh1, n_classes]))
 	}
 	biases = {
-	    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+	    'b1': tf.Variable(tf.random_normal([nh1])),
 	    'out': tf.Variable(tf.random_normal([n_classes]))
 	}
 
@@ -63,8 +63,6 @@ def main():
 	            batch_x, batch_y = mnist.train.next_batch(batch_size)
 	            _, c = sess.run([optimizer, cost], feed_dict={x: batch_x,y: batch_y})
 	            avg_cost += c / total_batch
-		    i=i+1
-	        # Display logs per epoch step
 	        if epoch % display_step == 0:
 	            print("Epoch:", '%04d' % (epoch+1), "cost=", \
 	                "{:.9f}".format(avg_cost))
