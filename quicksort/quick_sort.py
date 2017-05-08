5cd280b02f7010cf60dc0b6f6b345a9c9575487b
Aashutosh Rathi <aashutoshrathi@gmail.com>
2017-05-08 20:38:19 +0530

    Added Randomized Pivot using 3 Pivot Technique in Quick Sort [Python] (#147). Closes #144
    * Quick Sort [Python] Randomize Pivot
    
    changed p to l
    
    Documentation of functions changed
    
    * Update quick_sort.py

--- quicksort/quick_sort.py
+++ quicksort/quick_sort.py
@@ -1,16 +1,39 @@
-def partition(array, p, r):
+def median(a, i, j, k):
+    """
+    Return median of 3 integers from array a.
+    :param a: Iterable of elements
+    :param i: start element index
+    :param j: end element index
+    :param k: middle element index
+    :return: return median of values at indices i, j and k.
+    """
+    if a[i] > a[j]:
+        if a[j] > a[k]:
+            return j
+        else:
+            return k
+    else:
+        if a[i] > a[k]:
+            return i
+        else:
+            return k
+
+
+def partition(array, l, r):
     """
     Perform Partition Operation on array.
     Time Complexity: Theta(nLogn)
     Auxiliary Space: O(n)
     :param array: Iterable of elements
-    :param p: pivot value for array
+    :param l: pivot value for array
     :param r: right limit of array
     :return: return q value for function, used in partitioning of array.
     """
-    i = p - 1
+    i = l - 1
+    pivot_index = median(array, l, r, (l+r) // 2)
+    array[pivot_index], array[r] = array[r], array[pivot_index]
     pivot = array[r]
-    for j in range(p, r):
+    for j in range(l, r):
         if array[j] <= pivot:
             i += 1
             array[i], array[j] = array[j], array[i]
@@ -19,24 +42,24 @@ def partition(array, p, r):
     return i
 
 
-def quick_sort(array, pivot, right):
+def quick_sort(array, left, right):
     """
     Perform sort using partition function.
     Time Complexity : O(nlog(n)).
     Space Complexity : O(n).
     :param array: Iterable of elements
-    :param pivot: used as left limit of quick sort
+    :param left: used as left limit of quick sort
     :param right: right limit for quick sort
     :return: no returns, sorts array
     """
-    if pivot < right:
-        q = partition(array, pivot, right)
-        quick_sort(array, pivot, q - 1)
-        quick_sort(array, q, right)
+    if left < right:
+        q = partition(array, left, right)
+        quick_sort(array, left, q - 1)
+        quick_sort(array, q + 1, right)
 
 
 def main():
-    a = [15, 19, 18, 26, 456, 87, 45, 480, 5]
+    a = [1, 2, 1, 2, 3, 1, 2, 2, 1]
     quick_sort(a, 0, len(a) - 1)
     print(a)
 
