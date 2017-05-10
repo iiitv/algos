2fbacd2e0e006546a0e39dfc8ac38f9c74aa4693
prakash <prakash@debian>
2017-05-11 04:16:53 +0530

    Added Rod Cutting Problem [Java]

---
+++ rod_cutting_problem/RodCutting.java
@@ -0,0 +1,26 @@
+/**
+Problem Statement: Given a rod of length n inches and an array of prices that contains prices of all pieces of size smaller than n. 
+Determine the maximum value obtainable by cutting up the rod and selling the pieces.
+
+Time Complexity: O(n^2)
+Space Complexity: O(n)
+*/
+
+public class RodCutting{
+ 
+   static int cutRod(int price[],int n){
+        int best_price[] = new int[n+1];
+        for(int i=1;i<best_price.length;i++)
+            best_price[i] = price[i-1];       
+        for (int i = 1; i<=n; i++)
+            for (int j = i; j < n+1; j++)
+                best_price[j] = Math.max(best_price[j], best_price[j-i] + best_price[i]);
+        return best_price[n];
+    }
+
+    public static void main(String args[]){
+        int price[] = new int[] {10, 52, 84, 93, 101, 17, 117, 20};
+        int n = price.length;
+        System.out.println("Maximum Obtainable Value is " + cutRod(price, n));
+    }
+}
