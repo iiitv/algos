1bb9e66eb97db3f03a2c74907492175f260fa0a5
prakash <prakash@debian>
2017-05-11 18:12:27 +0530

    Update README.md

--- rod_cutting_problem/RodCutting.java
+++ rod_cutting_problem/RodCutting.java
@@ -6,21 +6,21 @@ Time Complexity: O(n^2)
 Space Complexity: O(n)
 */
 
-public class RodCutting{
- 
-   static int cutRod(int price[],int n){
-        int best_price[] = new int[n+1];
-        for(int i=1;i<best_price.length;i++)
-            best_price[i] = price[i-1];       
-        for (int i = 1; i<=n; i++)
+public class RodCutting {
+
+    public static int cutRod(int[] price) {
+        int n = price.length;
+        int[] best_price = new int[n+1];
+        for (int i = 1;i < best_price.length;i++)
+            best_price[i] = price[i-1];
+        for (int i = 1; i <= n; i++)
             for (int j = i; j < n+1; j++)
                 best_price[j] = Math.max(best_price[j], best_price[j-i] + best_price[i]);
         return best_price[n];
     }
 
-    public static void main(String args[]){
-        int price[] = new int[] {10, 52, 84, 93, 101, 17, 117, 20};
-        int n = price.length;
-        System.out.println("Maximum Obtainable Value is " + cutRod(price, n));
+    public static void main(String[] args) {
+        int[] price = new int[] {10, 52, 84, 93, 101, 17, 117, 20};
+        System.out.println("Maximum Obtainable Value is " + cutRod(price));
     }
 }
