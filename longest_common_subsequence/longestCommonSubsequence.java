427bb5bd8cfc9c0de6381b8dbd00486b905a4f33
TarunISCO <201552063@iiitvadodara.ac.in>
2017-05-14 20:13:55 +0530

    hope final review now

--- longest_common_subsequence/longestCommonSubsequence.java
+++ longest_common_subsequence/longestCommonSubsequence.java
@@ -2,19 +2,18 @@
 class LongestCommonSubsequence
 {
 	//Function that returns Longest Common Subsequence of two strings
-	//Time Complexity - O( len(str1) * len(str2) )
-	//Space Complexity 
+	//Time Complexity - O( len(str1) * len(str2) ) 
 	private static String lcs(String str1, String str2)
 	{
-		int [][] arr = new int[str1.length()+1][str2.length()+1];
+		int [][] arr=new int[str1.length()+1][str2.length()+1];
 		for(int i=str1.length()-1;i>=0;i--)
 		{
 			for(int j=str2.length()-1;j>=0;j--)
 			{
 				if(str1.charAt(i)==str2.charAt(j))
-					arr[i][j] = arr[i+1][j+1]+1;
+					arr[i][j]=arr[i+1][j+1]+1;
 				else
-					arr[i][j] = Math.max(arr[i+1][j], arr[i][j+1]);
+					arr[i][j]=Math.max(arr[i+1][j], arr[i][j+1]);
 			}
 		}
 		String res="";
@@ -24,11 +23,11 @@ class LongestCommonSubsequence
 		{
 			if(str1.charAt(i)==str2.charAt(j))
 			{
-				res = res + str1.charAt(i);
+				res=res+str1.charAt(i);
 				i++;
 				j++;
 			}
-			else if(arr[i+1][j] >= arr[i][j+1])
+			else if(arr[i+1][j]>=arr[i][j+1])
 				i++;
 			else
 				j++;
