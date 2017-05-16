c54cb74f591da489e99f3353cc2fe3b145a221c7
hims1911 <sharmahimanshu1911@gmail.com>
2017-05-16 08:09:33 -0700

    Added prims

---
+++ prims/prims.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+
+# define SIZE 5
+# define INFINITY 32767
+
+// This function finds the minimal spanning tree by Prim's Algorithm
+void prims(int G[SIZE][SIZE], int *parent) {
+    int select[SIZE], i, j, k;
+    int min_dist, v1, v2;
+    for (i = 0; i < SIZE; ++i)  // Initialize the selected vertices list
+        select[i] = 0;
+    select[0] = 1;
+    for (k = 1; k < SIZE; ++k) {
+        min_dist = INFINITY;
+        for (i = 0; i < SIZE; ++i) {  // Select an edge such that one vertex is selected and other is not and the edge
+            for (j = 0; j < SIZE; ++j) {  // has the least weight.
+                if (G[i][j] && ((select[i] && !select[j]) || (!select[i] && select[j]))) {
+                    if (G[i][j] < min_dist) {  //obtained edge with minimum wt 
+                        min_dist = G[i][j];
+                        parent[j] = i;
+                        v2 = j;   //picking up those vertices
+                    }
+                }
+            }
+        }
+        select[v1] = select[v2] = 1; 
+    }
+}
+
+void main() {
+    int G[SIZE][SIZE] = {{0, 2, 0, 6, 0},
+                         {2, 0, 3, 8, 5},
+                         {0, 3, 0, 0, 7},
+                         {6, 8, 0, 0, 9},
+                         {0, 5, 7, 9, 0},
+                        };
+    int i, j;
+    int parent[SIZE];
+    memset(parent, 0, SIZE);
+    printf("Edge\tWeight\n");
+    prims(G,parent);
+    for (i = 1; i < SIZE; ++i)
+      printf("%d - %d\t%d \n", parent[i], i, G[i][parent[i]]);
+}
