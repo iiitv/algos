7e6b2b2a8376cb42746536dc6278fbee5823ca66
jsroyal <jsroyal55@gmail.com>
2017-05-29 23:49:33 +0530

    update with source and destination

--- breadth_first_traversal/BreadthFirstTraversal.java
+++ breadth_first_traversal/BreadthFirstTraversal.java
@@ -9,30 +9,39 @@ public class BreadthFirstSearch {
     public static LinkedList<Integer>[] adj;
 
     //Function to add an edge into the breadthFirstTraversal
-    private static void addEdge(int v, int w) {
-        adj[v].add(w);
+    private static void addEdge(int source, int w) {
+        adj[source].add(w);
     }
 
-    private static void breadthFirstTraversal(int v) {
+    private static void breadthFirstTraversal(int source, int destination) {
         // false by default in java)
         Set<Integer> visited = new HashSet();
         // Mark the current node as visited
-        visited.add(v);
+        visited.add(source);
         LinkedList<Integer> queue = new LinkedList<>();
-        queue.add(v);
+        queue.add(source);
         while (queue.size() != 0) {
-            v = queue.poll();
-            System.out.println(v);
-            Iterator<Integer> i = adj[v].listIterator();
+            source = queue.poll();
+            System.out.println(source);
+            Iterator<Integer> i = adj[source].listIterator();
+            int flag = 0;
             while (i.hasNext()) {
                 int n = i.next();
+                if (n == destination) {
+                    System.out.println(n);
+                    flag = 1;
+                    break;
+                }
                 if (!visited.contains(n)) {
                     visited.add(n);
                     queue.add(n);
                 }
             }
+            if (flag == 1) {
+                break;
+            }
         }
-    }
+    }   
 
     public static void initEdges(int n) {
         adj = new LinkedList[n];
@@ -49,8 +58,7 @@ public class BreadthFirstSearch {
         addEdge(2, 1);
         addEdge(2, 3);
         addEdge(3, 3);
-        System.out.println("Breadth First Traversal starting from vertex 0");
-        breadthFirstTraversal(0);
+        System.out.println("Breadth First Traversal starting from source to destination");
+        breadthFirstTraversal(1,3);
     }
 }
-
