4701d1de6ce38f98b3891d11edc3f1db54061973
Aashutosh Rathi <aashutoshrathi@gmail.com>
2017-06-09 23:02:20 +0530

    final changes

--- rod_cutting_problem/rod_cutting.py
+++ rod_cutting_problem/rod_cutting.py
@@ -19,8 +19,8 @@ def rod_cutting(price, rod_len):
     opt_price = [0] * (rod_len + 1)
 
     for i in range(1, rod_len + 1):
-        for j in range(i):
-            opt_price[i] = max(max(opt_price), price[j] + opt_price[i - j - 1])
+        opt_price[i] = max(
+                    [-1] + [price[j] + opt_price[i - j - 1] for j in range(i)])
     return opt_price[rod_len]
 
 
