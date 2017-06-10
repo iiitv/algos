7932547364c712d065339a37ef643439607e859d
Andreas <andreasagap@yahoo.gr>
2017-06-10 23:18:50 +0300

    Create CoinChange.java

--- coin_change_problem/CoinChange.java
+++ coin_change_problem/CoinChange.java
@@ -1,11 +1,11 @@
 /*
  * Implementation of famous dynamic programming problem
- *  that aims to find out the maximum number of ways in
- *  which a value can be achieved using some fixed valued
- *  coins.
+ * that aims to find out the maximum number of ways in
+ * which a value can be achieved using some fixed valued
+ * coins.
  *
  * In the implementation, the time complexity is O(mn)
- *  and extra space required is O(n).
+ * and extra space required is O(n).
  */
 import java.util.Arrays;
 /*
