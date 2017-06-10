6e2ac73a023e7270efbc47fc00aac592c00106a7
Aashutosh Rathi <aashutoshrathi@gmail.com>
2017-06-10 20:30:25 +0530

    added new variable for len(price)

--- rod_cutting_problem/rod_cutting.py
+++ rod_cutting_problem/rod_cutting.py
@@ -15,12 +15,13 @@ def rod_cutting(price):
     :param price: List in which price[i] denotes price of rod of length i.
     :return: returns optimal solution for rod of length len(price).
     """
-    opt_price = [0] * (len(price) + 1)
+    length = len(price)
+    opt_price = [0] * (length + 1)
 
-    for i in range(1, len(price) + 1):
+    for i in range(1, length + 1):
         opt_price[i] = max(
                     [-1] + [price[j] + opt_price[i - j - 1] for j in range(i)])
-    return opt_price[len(price)]
+    return opt_price[length]
 
 
 def main():
