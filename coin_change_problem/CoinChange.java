4691aba12da2866ef0915c44810d982ad9f61526
Andreas <andreasagap@yahoo.gr>
2017-06-10 23:14:05 +0300

    Create CoinChange.java

--- coin_change_problem/CoinChange.java
+++ coin_change_problem/CoinChange.java
@@ -8,33 +8,30 @@
  *  and extra space required is O(n).
  */
 import java.util.Arrays;
-
 /*
  * coin: Array containing value of coins
  * m: Number of types of coins
  * n: Value to find the change for
  */
 public class CoinChange {
-	
-    static int coin_change_problem(int[] C, int m, int n){
-	int[] possibilities = new int[n+1];
-        Arrays.fill(possibilities, 0);
-        possibilities[0] = 1;
-        // Build the possibilities table in bottom-up manner
-	// For all coins,
-	// Update array if the current coin is capable of
-        // incrementing the possibility
-        for(int i = 0; i < m; i++){
-            for(int j = C[i]; j <= n; j++){
-                possibilities[j] += possibilities[j - C[i]];
-            }
-        }
-        return possibilities[n];
-    }
-    public static void main(String[] args) {
-        int[] coin = {2, 5, 3, 6};
-	int n = 10, m = coin.length;
-	System.out.println(coin_change_problem(coin, m, n));
-    }
-    
+	static int coin_change_problem(int[] C, int m, int n) {
+		int[] possibilities = new int[n+1];
+		Arrays.fill(possibilities, 0);
+		possibilities[0] = 1;
+		// Build the possibilities table in bottom-up manner
+		// For all coins,
+		// Update array if the current coin is capable of
+		// incrementing the possibility
+		for(int i = 0; i < m; i++){
+			for(int j = C[i]; j <= n; j++){
+				possibilities[j] += possibilities[j - C[i]];
+			}
+		}
+		return possibilities[n];
+	}
+	public static void main(String[] args) {
+		int[] coin = {2, 5, 3, 6};
+		int n = 10, m = coin.length;
+		System.out.println(coin_change_problem(coin, m, n));
+	}
 }
