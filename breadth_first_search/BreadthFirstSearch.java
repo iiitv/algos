88d8fc4475cb2c064f4aac36ea993ac7cf34532d
jsroyal <jsroyal55@gmail.com>
2017-06-12 00:21:09 +0530

    generic type input

--- breadth_first_search/BreadthFirstSearch.java
+++ breadth_first_search/BreadthFirstSearch.java
@@ -1,37 +1,37 @@
 import java.util.*;
 
-public class BreadthFirstSearch {
+public class BreadthFirstSearch <T> {
 
-    // Array  of lists for Adjacency List Representation
-    public static LinkedList<Integer>[] adj;
+    // HasMap  of lists for Adjacency List Representation
+    public HashMap<Integer, ArrayList<T>> adj;
 
     //Function to add an edge into the breadthFirstTraversal
-    private static void addEdge(int source, int w) {
-        adj[source].add(w);
+    private void addEdge(int source, T destination) {
+        adj.get (source).add (destination);
     }
 
-    public static ArrayList breadthFirstSearch(int source, int destination) {
+    public ArrayList breadthFirstSearch(T source, T destination) {
         // false by default in java)
         ArrayList arrayList = new ArrayList();
-        Set<Integer> visited = new HashSet();
+        Set<T> visited = new HashSet();
         // Mark the current node as visited
         visited.add(source);
-        LinkedList<Integer> queue = new LinkedList<>();
+        ArrayList<T> queue = new ArrayList<> ();
         queue.add(source);
         while (queue.size() != 0) {
-            source = queue.poll();
+            source = queue.remove(0);
             arrayList.add(source);
-            Iterator<Integer> i = adj[source].listIterator();
+            Iterator<T> i = adj.get(source).listIterator();
             int flag = 0;
             while (i.hasNext()) {
-                int n = i.next();
+                T n = i.next();
                 if (n == destination) {
                     arrayList.add(n);
                     flag = 1;
                     break;
                 }
                 if (!visited.contains(n)) {
-                    visited.add(n);
+                    visited.add(n);    // mark as visited.
                     queue.add(n);
                 }
             }
@@ -41,24 +41,25 @@ public class BreadthFirstSearch {
         }
         return arrayList;
     }
-
-    public static void initEdges(int n) {
-        adj = new LinkedList[n];
-        for (int i = 0; i < n; ++i) {
-            adj[i] = new LinkedList();
+    //init edges
+    public void initEdges(int n) {
+        adj = new HashMap<Integer, ArrayList<T>>();
+        for (int i = 0; i < n; i++) {
+            adj.put (i, new ArrayList<> ());
         }
     }
 
     public static void main(String[] args) {
-        initEdges(4);
-        addEdge(0, 1);
-        addEdge(0, 3);
-        addEdge(1, 2);
-        addEdge(2, 1);
-        addEdge(2, 3);
-        addEdge(3, 3);
+
+        BreadthFirstSearch<Integer> obj = new BreadthFirstSearch<> ();
+        obj.initEdges(4);
+        obj.addEdge(0, 1);
+        obj.addEdge(0, 3);
+        obj.addEdge(1, 2);
+        obj.addEdge(2, 1);
+        obj.addEdge(2, 3);
         System.out.println("Breadth First Search starting from source to destination");
-        ArrayList arrayList = breadthFirstSearch(1,3);
+        ArrayList arrayList = obj.breadthFirstSearch(1,3);
         System.out.println(arrayList);
     }
 }
