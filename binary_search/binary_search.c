6e764add74af397e0f209e22d6cce777beee0216
Harsh Khatore <HarshKhatore@users.noreply.github.com>
2017-06-11 23:43:31 +0530

    Updated binary_search.c [ C ]
    Added recursive implementation to binary_search.c [ C ]

--- binary_search/binary_search.c
+++ binary_search/binary_search.c
@@ -5,6 +5,37 @@
  * @arr_size Size of array
  * @search_element Element to be searched
  */
+ 
+ /* function to perform recursive binary search
+ input parameters:
+ ar - user-input array.
+ l - left index of array.
+ r - right index of array.
+ ele - element to be searched.
+ */
+ int binarySearch(const int *ar, int l, int r, int ele)
+ {
+    if (r >= l)
+    {
+         int mid = l + (r - l)/2;    //calculate mid point of the array
+  
+         // If the element is present at the middle itself
+         
+         if (ar[mid] == ele){  return mid;   }
+  
+         // If element is smaller than mid, then it can only be present
+         // in left subarray
+         if (ar[mid] > ele){ return binarySearch(ar, l, mid-1, ele);}
+  
+         // Else the element can only be present in right subarray
+         return binarySearch(ar, mid+1, r, ele);
+    }
+  
+    // We reach here when element is not present in array
+    return -1;
+ }
+
+//Function to perform iterative binary search
 int binary_search(const int *arr, int arr_size, int search_element) {
 	int left = 0, right = arr_size - 1;
 
@@ -25,13 +56,19 @@ int binary_search(const int *arr, int arr_size, int search_element) {
 int main() {
 	int arr[] = {1, 5, 35, 112, 258, 324},
 		search_arr[] = {1, 35, 112, 324, 67},
-		pos, i;
+		pos, i, pos_rec;
 
 	for (i = 0 ; i < 5 ; i ++) {
 		pos = binary_search(arr, 6, search_arr[i]);
-
+		pos_rec = BinarySearch(arr, 0, 5, search_arr[i]);
+		
 		if (pos >= 0)
 			printf("%d found at index %d.\n", search_arr[i], pos);
+		else
+			printf("%d not found.\n", search_arr[i]);
+		
+		if(pos_rec >= 0)
+			printf("%d found at index %d, recursively\n", search_arr[i], pos);
 		else
 			printf("%d not found.\n", search_arr[i]);
 	}
