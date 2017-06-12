d1afa13b6f84e604eedd7125100f04d5fb747aec
jsroyal <jsroyal55@gmail.com>
2017-06-12 09:41:35 +0530

    minor fix to improve

--- breadth_first_search/BreadthFirstSearch.java
+++ breadth_first_search/BreadthFirstSearch.java
@@ -6,27 +6,28 @@ public class BreadthFirstSearch <T> {
     public HashMap<Integer, ArrayList<T>> adj;
 
     //Function to add an edge into the breadthFirstTraversal
-    private void addEdge(int source, T destination) {
-        adj.get (source).add (destination);
+    private void addEdge(int index, T destination) {
+        adj.get(index).add(destination);
     }
 
-    public ArrayList breadthFirstSearch(T source, T destination) {
+    public ArrayList breadthFirstSearch(T index, T destination) {
         // false by default in java)
-        ArrayList arrayList = new ArrayList();
+        ArrayList bfsPath = new ArrayList();
         Set<T> visited = new HashSet();
         // Mark the current node as visited
-        visited.add(source);
+        visited.add(index);
         ArrayList<T> queue = new ArrayList<> ();
-        queue.add(source);
+        queue.add(index);
         while (queue.size() != 0) {
-            source = queue.remove(0);
-            arrayList.add(source);
-            Iterator<T> i = adj.get(source).listIterator();
+            index = queue.remove(0);
+            bfsPath.add(index);
+            Iterator<T> i = adj.get(index).listIterator();
+            // after reach the target node break out the all loop
             int flag = 0;
             while (i.hasNext()) {
                 T n = i.next();
-                if (n == destination) {
-                    arrayList.add(n);
+                if (n.equals (destination)) {
+                    bfsPath.add(n);
                     flag = 1;
                     break;
                 }
@@ -34,32 +35,31 @@ public class BreadthFirstSearch <T> {
                     visited.add(n);    // mark as visited.
                     queue.add(n);
                 }
-            }
-            if (flag == 1) {
-                break;
+                if (flag == 1) {
+                    break;
+                }
             }
         }
-        return arrayList;
+        return bfsPath;
     }
     //init edges
     public void initEdges(int n) {
         adj = new HashMap<Integer, ArrayList<T>>();
         for (int i = 0; i < n; i++) {
-            adj.put (i, new ArrayList<> ());
+            adj.put(i, new ArrayList<> ());
         }
     }
 
     public static void main(String[] args) {
-
-        BreadthFirstSearch<Integer> obj = new BreadthFirstSearch<> ();
+        BreadthFirstSearch<Integer> obj = new BreadthFirstSearch<>();
         obj.initEdges(4);
         obj.addEdge(0, 1);
         obj.addEdge(0, 3);
         obj.addEdge(1, 2);
         obj.addEdge(2, 1);
         obj.addEdge(2, 3);
-        System.out.println("Breadth First Search starting from source to destination");
-        ArrayList arrayList = breadthFirstSearch(1, 3);
-        System.out.println(arrayList);
+        System.out.println("Breadth First Search starting from index to destination");
+        ArrayList bfsPath = obj.breadthFirstSearch(1, 3);
+        System.out.println(bfsPath);
     }
 }
