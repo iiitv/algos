f7b1159c8f35ee586bace9db1c4de7a0dddf358c
TarunISCO <201552063@iiitvadodara.ac.in>
2017-06-14 12:07:56 +0530

    issues in readme solved

--- insertion_sort/insertion_sort.py
+++ insertion_sort/insertion_sort.py
@@ -6,8 +6,8 @@ def insertionSort(alist):
 	for index in range(1, len(alist)):
 		current_value = alist[index]
 		position = index
-		while position > 0 and alist[position-1] > current_value:
-			alist[position] = alist[position-1]
+		while position > 0 and alist[position - 1] > current_value:
+			alist[position] = alist[position - 1]
 			position -= 1
 			alist[position] = current_value
 
