c32c50c78bb64f390927e9fdd8f3cce7499e1e3f
Anshuman Verma <anshu.av97@gmail.com>
2017-06-16 00:53:43 +0530

    Added codacy suggestions

--- linear_search/linear_search.js
+++ linear_search/linear_search.js
@@ -1,25 +1,28 @@
-function linear_search (arr, x) {
+function linearSearch (arr, x) {
 	/*
 	Performs a linear search
 	:param arr: List of elements to search from
 	:param x: Element to search for
 	:return: Index if element found else -1
 	*/
-	for (let it in arr)
-		if (arr[it] == x)
+	for (let it in arr) {
+		if (arr[it] === x) {
 			return it;
+		}
+	}
 	return -1;
 }
 
 function main () {
-	A = [];
-	for (let i = 1; i<=100; i++)
+	let A = [];
+	for (let i = 1; i<=100; i++) {
 		A.push(i);
-	index = linear_search(A, 12);
-	if (index == -1) {
-		console.log("Element was not found");
+	}
+	index = linearSearch(A, 12);
+	if (index === -1) {
+		console.log('Element was not found');
 	} else {
-		console.log("Element was found at %dth index", index);
+		console.log('Element was found at %dth index', index);
 	}
 }
 
