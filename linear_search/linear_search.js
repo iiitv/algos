37f2e0e283227c6e71c4817245c03df98bb4482f
Anshuman Verma <anshu.av97@gmail.com>
2017-06-16 00:33:02 +0530

    improve indentation

--- linear_search/linear_search.js
+++ linear_search/linear_search.js
@@ -1,9 +1,9 @@
 function linear_search (arr, x) {
 	/*
 	Performs a linear search
-    :param arr: List of elements to search from
-    :param x: Element to search for
-    :return: Index if element found else -1
+	:param arr: List of elements to search from
+	:param x: Element to search for
+	:return: Index if element found else -1
 	*/
 	for (let it in arr)
 		if (arr[it] == x)
