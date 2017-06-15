11d9abd653e2279847f3c171704cd7a1f7317a7a
rashinanwani <anilnanwani717@gmail.com>
2017-06-16 05:06:12 +0530

    Added Prime Factor [Java] Fixes #283

---
+++ prime_factor/PrimeFactor.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class PrimeFactor {
+    public static ArrayList<Integer> primeFactor(int n)
+    {
+        ArrayList<Integer> primeNo = new ArrayList<Integer>();
+        for (int i = 2; i <=n; i++)
+        {
+            if ( n % i == 0)
+            {
+                primeNo.add(i);
+                while (n % i == 0) {
+                    n = n / i;
+                }
+            }
+        }
+        return primeNo;
+    }
+
+    public static void main(String[] args) {
+        int n = 8;
+        ArrayList<Integer> primes = primeFactor(n);
+        System.out.println("Prime Factors are:");
+        for (Integer i : primeFactor(n))
+        {
+            System.out.println(i);
+        }
+    }}
