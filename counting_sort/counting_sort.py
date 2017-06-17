2da392ffb3f8a66741c7cbc8117c8a802c58db85
khilanravani <khilanravani@gmail.com>
2017-06-17 15:48:35 +0530

    Update counting_sort.py

--- counting_sort/counting_sort.py
+++ counting_sort/counting_sort.py
@@ -1,11 +1,11 @@
-def countOccurences(a, c):
-    for i in range(len(a)):
+def count_occurences(a, c):
+    for i in a:
         c[a[i]] += 1
     for i in range(1, len(c)):
         c[i] = c[i - 1] + c[i]
 
 
-def countingSort(a, k):
+def counting_Sort(a, k):
     b = [0] * len(a)
     c = [0] * (k + 1)
     countOccurences(a, c)
@@ -17,13 +17,8 @@ def countingSort(a, k):
 
 def main():
     A = [3, 4, 5, 3, 6, 3, 3, 5, 5, 3, 2, 1, 5, 8]
-    k = 0
-    for i in range(len(A)):
-        if k < A[i]:
-            k = A[i]
-    A = countingSort(A, k)
-    print
-    A
+    k = max(A)
+    print(counting_sort(A, k))
 
 
 if __name__ == '__main__':
