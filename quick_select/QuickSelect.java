b8eae07a0aa13f57a712e7672c3532c09c090aa7
Andreas <andreasagap@yahoo.gr>
2017-06-17 11:05:11 +0300

    Minor changes

--- quick_select/QuickSelect.java
+++ quick_select/QuickSelect.java
@@ -1,32 +1,32 @@
 public class QuickSelect {
     /*
     * partition function
-    * array        : array on which partitioning has to be done
+    * array     : array on which partitioning has to be done
     * left      : left index of the partitioning subarray
     * right     : right index of the partitioning subarray
-    * pivot_idx : pivot index from which partition has to done
-    * store_idx : index of the last element of the left subarray
+    * pivotIndex : pivot index from which partition has to done
+    * storeIndex : index of the last element of the left subarray
     * return    : the index of the last element of the left subarray
      */
-    public static int partition(int[] array, int left, int right, int pivot_idx) {
-        int pivot_value = array[pivot_idx];
+    private static int partition(int[] array, int left, int right, int pivotIndex) {
+        int pivotValue = array[pivotIndex];
         int temp = array[right];
-        array[right] = array[pivot_idx];
-        array[pivot_idx] = temp;
-        int store_idx = left;
-        while (left < right){
-            if (array[left] < pivot_value){
-                temp = array[store_idx];
-                array[store_idx] = array[left];
+        array[right] = array[pivotIndex];
+        array[pivotIndex] = temp;
+        int storeIndex= left;
+        while (left < right) {
+            if (array[left] < pivotValue) {
+                temp = array[storeIndex];
+                array[storeIndex] = array[left];
                 array[left] = temp;
-                store_idx++;
+                storeIndex++;
             }
             left++;
         }
         temp = array[right];
-        array[right] = array[store_idx];
-        array[store_idx] = temp;
-        return store_idx;
+        array[right] = array[storeIndex];
+        array[storeIndex] = temp;
+        return storeIndex;
     }
 
     /*
@@ -34,21 +34,25 @@ public class QuickSelect {
     * left        : left index of the subarray
     * right       : right index of the subarray
     * pos         : position to find the element using quick sort
-    * pivot_index : pivot index
+    * pivotIndex  : pivot index
     * return      : the value of element at pos place in the sorted array
      */
     public static int quickSelect(int[] array, int left, int right, int pos) {
-        int pivot_index;
-        if (left == right)
+        int pivotIndex;
+        if (left == right) {
             return array[left];
-        pivot_index = right - 1;
-        pivot_index = partition(array, left, right, pivot_index);
-        if (pos == pivot_index)
-            return array[pivot_index];
-        else if (pos < pivot_index)
-            return quickSelect(array, left, pivot_index - 1, pos);
-        else 
-            return quickSelect(array, pivot_index + 1, right, pos);
+        }
+        pivotIndex = right - 1;
+        pivotIndex = partition(array, left, right, pivotIndex);
+        if (pos == pivotIndex) {
+            return array[pivotIndex];
+        }
+        else if (pos < pivotIndex) {
+            return quickSelect(array, left, pivotIndex - 1, pos);
+        }
+        else {
+            return quickSelect(array, pivotIndex + 1, right, pos);
+        }
     }
 
     public static void main(String[] args) {
