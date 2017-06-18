76e856c8c31686f846568746ebc6ce7b434ed5a8
rashinanwani <anilnanwani717@gmail.com>
2017-06-18 21:33:52 +0530

    Added Prime Factor [Javascript] Fixes #314

---
+++ prime_factor/prime_factor.js
@@ -0,0 +1,21 @@
+function primeFactor (n) {
+	
+	for (let i = 2; i <= n; i++) {
+		if (n%i==0) {
+			console.log(i);
+			while(n%i==0) {
+        n=n/i;
+      }
+	  }
+	}
+}
+
+function main () {
+	let n=8;
+  console.log('Prime Factors are:');
+  for (let i = 2;i <= primeFactor(n);i++ ) {
+    console.log(i);
+  }
+}
+
+main();
