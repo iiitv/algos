69f45503c725f8beb27a3ccf5b3210b2b472cc96
Anshuman Verma <anshu.av97@gmail.com>
2017-06-18 23:53:05 +0530

    use proper function names

--- euclidean_gcd/euclidean_gcd.js
+++ euclidean_gcd/euclidean_gcd.js
@@ -1,12 +1,12 @@
-function euclidean_gcd_recursive(first, second) {
+function euclideanGcdRecursive(first, second) {
 	if(second === 0) {
 		return first;
 	} else {
-		return euclidean_gcd_recursive(second, (first % second));
+		return euclideanGcdRecursive(second, (first % second));
 	}
 }
 
-function euclidean_gcd(first, second) {
+function euclideanGcd(first, second) {
 	while(second !== 0) {
 		let temp = second;
 		second = first % second;
@@ -18,8 +18,8 @@ function euclidean_gcd(first, second) {
 function main() {
 	let first = 20;
 	let second = 30;
-	console.log('Recursive GCD for %d and %d is %d', first, second ,euclidean_gcd_recursive(first, second));
-	console.log('Iterative GCD for %d and %d is %d', first, second, euclidean_gcd(first, second));
+	console.log('Recursive GCD for %d and %d is %d', first, second ,euclideanGcdRecursive(first, second));
+	console.log('Iterative GCD for %d and %d is %d', first, second, euclideanGcd(first, second));
 }
 
 main();
