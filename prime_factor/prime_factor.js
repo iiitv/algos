70786b7fc409b693aeb20cc6c089409e2c7dc2f6
rashinanwani <anilnanwani717@gmail.com>
2017-06-19 00:39:51 +0530

    changes

--- prime_factor/prime_factor.js
+++ prime_factor/prime_factor.js
@@ -2,7 +2,7 @@ function primeFactor (n) {
 	for (let i = 2; i <= n; i++) {
 		if (n % i === 0) {
 			console.log(i);
-			while(n % i === 0) {
+			while (n % i === 0) {
 				n = n / i;
 			}
 		}
@@ -12,7 +12,7 @@ function primeFactor (n) {
 function main () {
 	let n = 8;
 	console.log('Prime Factors are:');
-	for (let i = 2; i <= primeFactor(n); i++ ) {
+	for (let i = 2; i <= primeFactor(n); i++) {
 		console.log(i);
 	}
 }
