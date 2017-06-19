229b920b4bdd3601d4219f42a5b869bac8914489
rashinanwani <anilnanwani717@gmail.com>
2017-06-19 20:27:15 +0530

    changes

--- prime_factor/prime_factor.js
+++ prime_factor/prime_factor.js
@@ -1,27 +1,31 @@
 function primeFactor (n) {
-	/*
-	Finding all the prime factors of given no
-	:param n: Number whose prime factors are going to be found
-	*/
-	for (let i = 2; i <= Math.sqrt(n); i++) {
-		if (n % i === 0) {
-			console.log(i);
-			while (n % i === 0) {
-				n = n / i;
-			}
-		}
-	}
-	if (n !== 1) {
-		console.log(n);
-	}
+  /*
+  Finding all the prime factors of given number
+  :param n: Number whose prime factors are going to be found
+  :returns: Array with the prime factors
+  */
+  let primes = []
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      primes.push(i);
+      while (n % i === 0) {
+        n = n / i;
+      }
+    }
+  }
+  if (n !== 1) {
+    primes.push(n);
+  }
+  return primes
 }
 
 function main () {
-	let n = 582;
-	console.log('Prime Factors are:');
-	for (let i = 2; i <= primeFactor(n); i++) {
-		console.log(i);
-	}
+  let n = 8;
+  console.log('Prime Factors are:');
+  let primes = primeFactor(n);
+  for (let i = 0; i < primes.length; i++) {
+    console.log(primes[i]);
+  }
 }
 
 main();
