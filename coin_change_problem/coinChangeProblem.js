ddec983e4f5b422ebcd3761ed75c810c404bc100
Anshuman Verma <anshu.av97@gmail.com>
2017-06-19 12:10:56 +0530

    Travix fix v1.1

--- coin_change_problem/coinChangeProblem.js
+++ coin_change_problem/coinChangeProblem.js
@@ -8,7 +8,7 @@ function coinChangeProblem (coins, amount) {
     * :param amount: It is value which is to be obtained with coins.
     * :return: returns maximum number of ways amount can be arranged in.
 	*/
-	let possibilities = new Array(amount+1);
+	let possibilities = new Array(amount + 1);
 	possibilities.fill(0);
 	possibilities[0] = 1;
 	for (let i = 0; i < coins.length; i++) {
