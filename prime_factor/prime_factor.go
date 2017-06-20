e307f1166cf1b62e20b5540f1f6366d15aa41d49
rashinanwani <anilnanwani717@gmail.com>
2017-06-20 17:38:37 +0530

    changes

--- prime_factor/prime_factor.go
+++ prime_factor/prime_factor.go
@@ -4,14 +4,15 @@ import (
 	"fmt"
 	"math"
 )
+
 // PrimeFactor finds all the prime factor of given number
 func PrimeFactor(n int) []int {
 	var primes []int
 	sqrt := int(math.Sqrt(float64(n)))
 	for i := 2; i <= sqrt; i++ {
-		if n % i == 0 {
+		if n%i == 0 {
 			primes = append(primes, i)
-			for n % i == 0 {
+			for n%i == 0 {
 				n = n / i
 			}
 		}
@@ -22,7 +23,7 @@ func PrimeFactor(n int) []int {
 	return primes
 }
 
-func main () {
+func main() {
 	n := 8
 	fmt.Println("Prime Factors are :")
 	fmt.Println(PrimeFactor(n))
