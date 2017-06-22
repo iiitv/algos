effc2430c9418eba5e863696160a99d3ca8a5e26
Anshuman Verma <anshu.av97@gmail.com>
2017-06-22 10:47:00 +0530

    Update comments

--- coin_change_problem/coinChangeProblem.js
+++ coin_change_problem/coinChangeProblem.js
@@ -1,12 +1,11 @@
 function coinChangeProblem (coins, amount) {
 	/*
 	* Find out maximum number of ways in which a amount can
-    * be obtained using fixed value coins.
-
-    * Time Complexity : O((type of coins)*amount)
-    * :param coins: Iterable of elements containing value of coins.
-    * :param amount: It is value which is to be obtained with coins.
-    * :return: returns maximum number of ways amount can be arranged in.
+	* be obtained using fixed value coins.
+	* Time Complexity : O((type of coins)*amount)
+	* :param coins: Iterable of elements containing value of coins.
+	* :param amount: It is value which is to be obtained with coins.
+	* :return: returns maximum number of ways amount can be arranged in.
 	*/
 	let possibilities = new Array(amount + 1);
 	possibilities.fill(0);
