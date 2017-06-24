a2cfacea8634166a24a9a4592661a03ab078a6b9
Monal Shadi <monalshadi16@gmail.com>
2017-06-24 16:46:13 +0530

    Fixed Java compilation in scripts Fixes #332

--- .bin/test_running.sh
+++ .bin/test_running.sh
@@ -26,7 +26,7 @@ echo ""
 echo "Testing Java files..."
 for i in $(ls -1 **/*.java); do
     echo "    Compiling $i - javac $i -d ."
-    javac $i -d .
+    javac -Werror -Xlint:all $i -d .
     filename="${i##*/}"
     classname="${filename%.*}"
     echo "    Running $i - java $classname > /dev/null"
