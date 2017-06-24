cae2159e34c8c7beca254f6b75328e6a1dae18a9
Anshuman Verma <anshu.av97@gmail.com>
2017-06-24 16:15:20 +0530

    Take that inches off  !

--- rod_cutting_problem/rodCuttingProblem.js
+++ rod_cutting_problem/rodCuttingProblem.js
@@ -1,5 +1,5 @@
 /*
-* Problem Statement: Given a rod of length n inches and an array of prices that contains prices of all pieces of size smaller than n.
+* Problem Statement: Given a rod of length n and an array of prices that contains prices of all pieces of size smaller than n.
 * Determine the maximum value obtainable by cutting up the rod and selling the pieces.
 */
 
