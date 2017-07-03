e3ee28be5e92b13a1a705c26727809dd973b4c63
Anshuman Verma <anshu.av97@gmail.com>
2017-07-03 14:17:58 +0530

    bot fix v1.0

--- insertion_sort/insertionSort.js
+++ insertion_sort/insertionSort.js
@@ -12,17 +12,17 @@ function insertionSort (arr) {
 	for (let i = 1; i < arr.length; i++) {
 		let j = i - 1;
 		let temp = arr[i];
-		while (j >= 0 && arr[j] > temp){
+		while (j >= 0 && arr[j] > temp) {
 			arr[j + 1] = arr[j];
 			j--;
 		}
-		arr[j+1] = temp;
+		arr[j + 1] = temp;
 	}
 	return arr;
 }
 
 function main () {
-	let arr = [5,9,3,1,99];
+	let arr = [5, 9, 3, 1, 99];
 	arr = insertionSort(arr);
 	console.log(arr);
 }
