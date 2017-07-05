d4f801c8f4ec5a95546e2e78155c564d5488dd99
Anshuman Verma <anshu.av97@gmail.com>
2017-07-05 22:07:08 +0530

    bot fix v1.0

--- longest_common_subsequence/longestCommonSubsequence.js
+++ longest_common_subsequence/longestCommonSubsequence.js
@@ -9,8 +9,9 @@ function longestCommonSubsequence (str1, str2) {
 	let len1 = str1.length;
 	let len2 = str2.length;
 	let lcs = new Array(len1 + 1);
-	for (let x = 0; x <= len1; x++)
+	for (let x = 0; x <= len1; x++) {
 		lcs[x] = new Array(len2 + 1);
+	}
 	for (let i = 0; i <= len1; i++) {
 		for (let j = 0; j <= len2; j++) {
 			if (i === 0 || j === 0) {
@@ -24,19 +25,18 @@ function longestCommonSubsequence (str1, str2) {
 			}
 		}
 	}
-	let totalLength = lcs[len1][len2];
-	let res = "";
+	let res = '';
 	let i = len1;
 	let j = len2;
 	while (i > 0 && j > 0) {
 		if (str1[i - 1] === str2[j - 1]) {
-			res = str1[i-1] + res;
+			res = str1[i - 1] + res;
 			i--;
 			j--;
 		}
 		else if (lcs[i - 1][j] > lcs[i][j - 1]) {
 			i--;
-		} 
+		}
 		else {
 			j--;
 		}
@@ -45,9 +45,9 @@ function longestCommonSubsequence (str1, str2) {
 }
 
 function main () {
- let a = 'whattheheckman';
- let b = 'whatareyoudoinghere';
- console.log('Longest Common Subsequence : ' + longestCommonSubsequence(a, b));
+	let a = 'whattheheckman';
+	let b = 'whatareyoudoinghere';
+	console.log('Longest Common Subsequence : ' + longestCommonSubsequence(a, b));
 }
 
 main();
