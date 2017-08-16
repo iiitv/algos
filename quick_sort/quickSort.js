e78c6284d85ee013ea596fa0341f0a104bd7aad6
rashinanwani <rashi.nanwani2000@gmail.com>
2017-08-16 17:39:05 +0530

    changes

--- quick_sort/quickSort.js
+++ quick_sort/quickSort.js
@@ -76,9 +76,8 @@ function quickSort (arr, left = 0, right = arr.length - 1) {
 
 function main () {
 	let arr = [2, 1, 6, 44, 8, 9, 10];
-	console.log('Sorted data is');
 	quickSort(arr);
-	console.log(arr);
+	console.log('Sorted data is',arr);
 }
 
 main();
