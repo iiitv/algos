107e17224383fd901a12c06290110eedb82875cb
jsroyal <jsroyal55@gmail.com>
2017-08-17 13:44:54 +0530

    added fibonacciNumber

---
+++ fibonacci_number/FibonacciNumber.java
@@ -0,0 +1,27 @@
+import java.util.*;
+/*
+ *Fibonacci series 1,1,2,3,5,8,13,21...
+ *Find the nth value of series.
+ */
+public class FibonacciNumber {
+	static int fibonacciNumber(int n) {
+		int sum = 0;
+		int b = 0;
+		int a = 1;		
+        if (n == 1){
+        	return 1;
+        }
+		for (int i = 1;i < n;i++){
+			sum = a + b;
+			b = a;
+			a = sum;	
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		System.out.println(fibonacciNumber(n));
+	}
+}
