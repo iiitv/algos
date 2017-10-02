8783bc6fcf8a410c7c862a829108da543229b064
ManBearPigg <sam-allick@hotmail.ca>
2017-10-02 16:53:40 -0400

    Update binary_search/binary_search.cpp
    Substituted spaces for tabs to satisfy the guidelines.

--- binary_search/binary_search.cpp
+++ binary_search/binary_search.cpp
@@ -15,21 +15,21 @@ using namespace std;
 // int: index of target if it exists, else -1.
 //------------------------------------------------
 int binary_search(vector<int> list, int target) {
-    int first = 0;
-    int last = list.size() - 1;
-    while (first <= last) {
-        int mid = (first + last) / 2;
-        if (list[mid] == target) {
-            return mid;
-        }
-        else if (list[mid] > target) {
-            last = mid - 1;
-        }
-        else {
-            first = mid + 1;
-        }
-    }
-    return -1;
+	int first = 0;
+	int last = list.size() - 1;
+	while (first <= last) {
+		int mid = (first + last) / 2;
+		if (list[mid] == target) {
+			return mid;
+		}
+		else if (list[mid] > target) {
+			last = mid - 1;
+		}
+		else {
+			first = mid + 1;
+		}
+	}
+	return -1;
 }
 
 
@@ -37,16 +37,16 @@ int binary_search(vector<int> list, int target) {
 // Test binary_search().
 //------------------------------------------------
 int main() {
-    vector<int> list;
-    list.push_back(1);
-    list.push_back(2);
-    list.push_back(5);
-    int target = 2;
-    int idx = binary_search(list, target);
-    if (idx == -1) {
-        cout << "Target not in list." << endl;
-    }
-    else {
-        cout << "Target is at index " << idx << endl;
-    }
+	vector<int> list;
+	list.push_back(1);
+	list.push_back(2);
+	list.push_back(5);
+	int target = 2;
+	int idx = binary_search(list, target);
+	if (idx == -1) {
+		cout << "Target not in list." << endl;
+	}
+	else {
+		cout << "Target is at index " << idx << endl;
+	}
 }
