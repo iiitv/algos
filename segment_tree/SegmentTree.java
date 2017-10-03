23dab09eedf053a035180d1a9ebba49002102ce0
Vishal Prakash <vishal.p@media.net>
2017-10-03 20:12:35 +0530

    Added Segment Tree in JAVA

--- segment_tree/SegmentTree.java
+++ segment_tree/SegmentTree.java
@@ -1,44 +1,34 @@
-public class SegmentTree
-{
+public class SegmentTree {
     private int[] tree;
-    private int maxsize;
-    private int height;
  
     private  final int STARTINDEX = 0; 
     private  final int ENDINDEX;
     private  final int ROOT = 0;
  
-    public SegmentTree(int size)
-    {
-        height = (int)(Math.ceil(Math.log(size) /  Math.log(2)));
-        maxsize = 2 * (int) Math.pow(2, height) - 1;
+    public SegmentTree(int size) {
+        int height = (int)(Math.ceil(Math.log(size) /  Math.log(2)));
+        int maxsize = 2 * (int) Math.pow(2, height) - 1;
         tree = new int[maxsize];
-        ENDINDEX = size - 1; 
+        ENDINDEX = size - 1;
     }
  
-    private int leftchild(int pos)
-    {
+    private int leftchild(int pos) {
         return 2 * pos + 1;
     }
  
-    private int rightchild(int pos)
-    {
+    private int rightchild(int pos) {
         return 2 * pos + 2;
     }
  
-    private int mid(int start, int end)
-    {
+    private int mid(int start, int end) {
         return (start + (end - start) / 2); 
     }
  
-    private int getSumUtil(int startIndex, int endIndex, int queryStart, int queryEnd, int current)
-    {
-        if (queryStart <= startIndex && queryEnd >= endIndex )
-        {
+    private int getSumUtil(int startIndex, int endIndex, int queryStart, int queryEnd, int current) {
+        if (queryStart <= startIndex && queryEnd >= endIndex ) {
             return tree[current];
         }
-        if (endIndex < queryStart || startIndex > queryEnd)
-        {
+        if (endIndex < queryStart || startIndex > queryEnd) {
             return 0;
         }
         int mid = mid(startIndex, endIndex);
@@ -46,19 +36,15 @@ public class SegmentTree
                  + getSumUtil( mid + 1, endIndex, queryStart, queryEnd, rightchild(current));
     }
  
-    public int getSum(int queryStart, int queryEnd)
-    {
-        if(queryStart < 0 || queryEnd > tree.length)
-        {
+    public int getSum(int queryStart, int queryEnd) {
+        if(queryStart < 0 || queryEnd > tree.length) {
             return -1;
         }
         return getSumUtil(STARTINDEX, ENDINDEX, queryStart, queryEnd, ROOT);
     }
  
-    private int constructSegmentTreeUtil(int[] elements, int startIndex, int endIndex, int current)
-    {
-        if (startIndex == endIndex)
-        {
+    private int constructSegmentTreeUtil(int[] elements, int startIndex, int endIndex, int current) {
+        if (startIndex == endIndex) {
             tree[current] = elements[startIndex];
             return tree[current];	
         }
@@ -68,35 +54,29 @@ public class SegmentTree
         return tree[current];
     }
  
-    public void constructSegmentTree(int[] elements)
-    {
+    public void constructSegmentTree(int[] elements) {
         constructSegmentTreeUtil(elements, STARTINDEX, ENDINDEX, ROOT);	
     }
  
-    private void updateTreeUtil(int startIndex, int endIndex, int updatePos, int update, int current)
-    {
-        if ( updatePos < startIndex || updatePos > endIndex)
-        {
+    private void updateTreeUtil(int startIndex, int endIndex, int updatePos, int update, int current) {
+        if ( updatePos < startIndex || updatePos > endIndex) {
             return;
         }
         tree[current] = tree[current] + update;
-        if (startIndex != endIndex)
-        {
+        if (startIndex != endIndex) {
             int mid = mid(startIndex, endIndex);
             updateTreeUtil(startIndex, mid, updatePos, update, leftchild(current));
             updateTreeUtil(mid+1, endIndex, updatePos, update, rightchild(current));
         }
     }
  
-    public void update(int update, int updatePos, int[] elements)
-    {
+    public void update(int update, int updatePos, int[] elements) {
         int updatediff = update - elements[updatePos]  ;
         elements[updatePos] = update;
         updateTreeUtil(STARTINDEX, ENDINDEX, updatePos, updatediff, ROOT);
     }
  
-    public static void main(String...arg)
-    {
+    public static void main(String...arg) {
         int[] elements = {1,3,5,7,9,11};
         SegmentTree segmentTree = new SegmentTree(6);
         segmentTree.constructSegmentTree(elements);
