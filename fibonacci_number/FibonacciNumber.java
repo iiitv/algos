c46d903d72221f5a1da7d70a0b0cc7d1d8ac463e
Jitendra Singh <jsroyal55@gmail.com>
2017-10-04 19:19:26 +0530

    Update FibonacciNumber.java

--- fibonacci_number/FibonacciNumber.java
+++ fibonacci_number/FibonacciNumber.java
@@ -4,7 +4,7 @@
  */
 
 public class FibonacciNumber {
-    
+
     public static int fibonacciNumber(int n) {
         int sum = 0;
         int b = 0;
@@ -12,13 +12,13 @@ public class FibonacciNumber {
         if (n == 1) {
             return 1;
         }
-	    for (int i = 1;i < n;i++) {
-		    sum = a + b;
-		    b = a;
-	        a = sum;
-	    }
-	    return sum;
-    }
+        for (int i = 1;i < n;i++) {
+            sum = a + b;
+            b = a;
+            a = sum;
+        }
+        return sum;
+        }
 
     public static void main(String[] args) {
         int n = 5;
