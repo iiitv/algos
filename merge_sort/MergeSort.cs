1e54e3fd9804db70ada33da35e1cc79d5b7364cd
DimaMukhin <dima9348@gmail.com>
2017-10-04 19:37:24 -0400

    Added method comments and time and space complexity

--- merge_sort/MergeSort.cs
+++ merge_sort/MergeSort.cs
@@ -1,7 +1,22 @@
+/*
+Time Complexity: O(n log(n))
+Space Complexity: O(n)
+*/
+
 using System;
 
 public class MergeSort
 {
+    /// <summary>
+    /// Merges two unsorted halves of an array into one sorted array
+    /// first half: from 'first' to 'mid'
+    /// second half: from 'mid' to 'last'
+    /// </summary>
+    /// <param name="a">The original unsorted array</param>
+    /// <param name="first">The start index of the first half of the array a</param>
+    /// <param name="mid">The middle index of the two halves of the array</param>
+    /// <param name="last">The end index of the second half of the array a</param>
+    /// <param name="temp">A temporary array for storing the sorted halves</param>
     public static void Merge(int[] a, int first, int mid, int last, int[] temp)
     {
         int currL = first;  //left half pointer
@@ -21,6 +36,14 @@ public class MergeSort
             a[currT] = temp[currT];
     }
 
+    /// <summary>
+    /// Recursive method for sorting an array 'a' between two indexes
+    /// first and last by using merge sort
+    /// </summary>
+    /// <param name="a">The original unsorted array</param>
+    /// <param name="first">The start index. The array will be sorted starting from this index</param>
+    /// <param name="last">The end index. The array will be sorted untill this index</param>
+    /// <param name="temp">A temporary array for storing merge results</param>
     public static void DoMergeSort(int[] a, int first, int last, int[] temp)
     {
         if (last - first > 1)
@@ -32,6 +55,10 @@ public class MergeSort
         }
     }
 
+    /// <summary>
+    /// Merge sort helper method. Calls the recursive Merge sort method
+    /// </summary>
+    /// <param name="a">The original unsorted array</param>
     public static void DoMergeSort(int[] a)
     {
         if (a == null || a.Length <= 1)
