42e8817fc16eccb59d5d76cce52fa0cd06385728
Yash Singh <raivaibhav54@gmail.com>
2017-10-09 17:44:08 +0530

    Selection_Sort: Add Selection_Sort
    Added Selection_Sort.rb

--- Selection_Sort/Selection_Sort.rb
+++ Selection_Sort/Selection_Sort.rb
@@ -1,21 +1,28 @@
-def SelectionSort(a)# passing array in SelectionSort fuction 
-  n = a.length - 1# n is a variable which holds the  length of array which we can be find  by using inbuilt 'length' function  
+# passing array in SelectionSort fuction 
+def SelectionSort(a)
+  # n is a variable which holds the  length of array which we can be find  by using inbuilt 'length' function
+  n = a.length - 1  
   i = 0
   while i <= n - 1
-    smallest = i#smallest variable will hold the smallest value
+    #smallest variable will hold the smallest value
+    smallest = i
     j = i + 1
     while j <= n
       smallest = j if a[j] < a[smallest]
       j += 1
     end
-    a[i], a[smallest] = a[smallest], a[i] if i != smallest #swap
+    #swap
+    a[i], a[smallest] = a[smallest], a[i] if i != smallest 
     i += 1
   end
 end
 
 #declare array 
-a = ([89,55,101,36].shuffle)# a is name of array and inbuilt function 'shuffle'will shuffle the values of array a
+# a is name of array and inbuilt function 'shuffle'will shuffle the values of array a
+a = ([89,55,101,36].shuffle)
 
-SelectionSort(a)#calling SelectionSort function
+#calling SelectionSort function
+SelectionSort(a)
 
-puts "Sorted array is: #{a.inspect}"#print the sorted array
+#print the sorted array
+puts "Sorted array is: #{a.inspect}"
