018ecc0b532a0cff986640a5e1b217cc24179033
Manish Kumar <manu1.uec2014@gmail.com>
2017-10-11 17:30:29 +0530

    Update longestIncreasingSubsequence.cpp

--- longest_increasing_subsequence/longestIncreasingSubsequence.cpp
+++ longest_increasing_subsequence/longestIncreasingSubsequence.cpp
@@ -1,29 +1,35 @@
 //Time: O(n*log(n))
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <cmath>
+#include <climits>
 using namespace std;
 
-int LIS(vector<int> A) {
-	if(A.size()<=1) return A.size();
-	vector<int> S;
-	S.push_back(A[0]);
-	for(int i=1; i<A.size(); i++){
-		if(A[i]>S[S.size()-1]) S.push_back(A[i]);
-		else{
-			int start=0;
-			int end=S.size()-1;
-			while(start<=end){
-				int mid=start+(end-start)/2;
-				if(S[mid]>=A[i])  end=mid-1;
-				else start=mid+1;
+int LIS ( vector<int> A ) {
+	if ( A.size() <= 1 ) return A.size();
+	vector< int > S;
+	S.push_back( A[0] );
+	for ( int i = 1; i < A.size(); i++ ) {
+		if(A[i] > S[S.size()-1]) S.push_back(A[i]);
+		else {
+			int start = 0;
+			int end = S.size()-1;
+			while( start <= end ) {
+				int mid = start+(end-start)/2;
+				if( S[mid] >= A[i] )  end=mid-1;
+				else start = mid+1;
 			}
-			S[end+1]=A[i];
+			S[end+1] = A[i];
 		}
 	}
 	return S.size();
 }
-int main(){
-	int n; cin >> n; // size of array for which LIS needs to be found
+int main() {
+	int n;
+	// size of array for which LIS needs to be found
+	cin >> n;
 	vector<int> V(n);
-	for(int i=0; i<n; i++) cin >> V[i]; //input of vector
+	for(int i=0; i<n; i++ ) cin >> V[i]; //input vector
 	cout << LIS(V) << endl;
 }
