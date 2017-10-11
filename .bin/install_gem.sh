e6fde37f59d25357c94d19509f45e4922f45bb39
Aashutosh Rathi <aashutoshrathi@gmail.com>
2017-10-11 13:51:33 +0530

    Changes ruby-2.3 to ruby-full Merge branch 'master' into ruby-bears Updated ruby source fixed directory issue cd .. Merge remote-tracking branch 'origin/ruby-bears' into ruby-bears

--- .bin/install_gem.sh
+++ .bin/install_gem.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+wget https://cache.ruby-lang.org/pub/ruby/2.4/ruby-2.4.2.tar.gz
+tar -xf ruby-2.4.2.tar.gz
+cd ruby*
+./configure
+make -j4
+sudo make install
+ruby -v
+cd ..
 sudo gem install $(grep -vE "^\s*#" gem-requirements.txt  | tr "\n" " ")
