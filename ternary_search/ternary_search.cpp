bd43fcfb6fdfd9bdbc6be757c0651d03eab01d70
张永超 <yongchao.zhang@tendcloud.com>
2017-10-16 11:48:26 +0800

    follow Google C++ Style

--- ternary_search/ternary_search.cpp
+++ ternary_search/ternary_search.cpp
@@ -11,51 +11,41 @@ using namespace std;
  *		x : element to be searched.
  * Time Complexity : O(max(input))
  */  
-int ternary_search (int ar[],int n, int left, int right, int x)
-{
+int ternary_search (int ar[],int n, int left, int right, int x){
 
-    if(left < 0 || right > n-1 || left > right)
-    {
+    if(left < 0 || right > n-1 || left > right){
         return -1;
     }
-    if(x == ar[left])
-    {
+    if(x == ar[left]){
         return left;
     }
 
-    if(x == ar[right])
-    {
+    if(x == ar[right]){
         return right;
     }
 
    // Update the two index left and right if the element is not found.
 
 
-    if(x < ar[left])
-    {
+    if(x < ar[left]){
         return ternary_search(ar,n,left-1,right,x);
     }
 
-    if (x > ar[left] && x < ar[right])
-    {
-
+    if (x > ar[left] && x < ar[right]){
         return ternary_search(ar,n,left+1,right-1,x);
     }
 
-    if(x > ar[right])
-    {
+    if(x > ar[right]){
         return ternary_search(ar,n,left,right+1,x);
     }
 }
 
 
-int main()
-{
+int main(){
 	int s = 12;
     int v[s];
     short x;
-    for(int i = 1; i <= s; i++)
-    {
+    for(int i = 1; i <= s; i++){
         v[i-1] = i;
     }
     cout << "Enter number for research:\n";
@@ -64,12 +54,9 @@ int main()
     int left = s/3;
     int right = (s/3)*2;
 
-    if(ternary_search(v,s,left-1,right-1,x) == -1)
-    {
+    if(ternary_search(v,s,left-1,right-1,x) == -1){
         cout<<"Number does not exist in array.\n";
-    }
-    else
-    {
+    }else{
         cout<<"The index is:"<<ternary_search(v,s,left-1,right-1,x)<<"\n";
     }
     return 0;
