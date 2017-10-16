a643653659db580d331d4a1afe5d0208ac8ae321
Robin <1933549736@qq.com>
2017-10-16 06:14:56 -0500

    update code style

--- ternary_search/ternary_search.cpp
+++ ternary_search/ternary_search.cpp
@@ -11,7 +11,7 @@ using namespace std;
  *		x : element to be searched.
  * Time Complexity : O(max(input))
  */  
-int ternary_search (int ar[],int n, int left, int right, int x){
+int ternary_search (int ar[], int n, int left, int right, int x){
 
     if(left < 0 || right > n - 1 || left > right){
         return -1;
@@ -26,18 +26,16 @@ int ternary_search (int ar[],int n, int left, int right, int x){
     }
 
    // Update the two index left and right if the element is not found.
-
-
     if(x < ar[left]){
-        return ternary_search(ar,n,left - 1,right,x);
+        return ternary_search(ar, n, left - 1, right, x);
     }
 
     if (x > ar[left] && x < ar[right]){
-        return ternary_search(ar,n,left + 1,right - 1,x);
+        return ternary_search(ar, n, left + 1, right - 1, x);
     }
 
     if(x > ar[right]){
-        return ternary_search(ar,n,left,right + 1,x);
+        return ternary_search(ar, n, left, right + 1, x);
     }
 }
 
@@ -45,20 +43,19 @@ int ternary_search (int ar[],int n, int left, int right, int x){
 int main(){
 	int space = 12;
     int ar[space];
-    short x;
+    short x = 6;
+	
     for(int i = 1; i <= space; i++){
         ar[i - 1] = i;
     }
-    cout << "Enter number for research:\n";
-    cin >> x;
 
     int left = space / 3;
     int right = (space / 3) * 2;
 
-    if(ternary_search(ar,space,left - 1,right - 1,x) == -1){
+    if(ternary_search(ar, space, left - 1, right - 1, x) == -1){
         cout << "Number does not exist in array.\n";
     }else{
-        cout << "The index is:" << ternary_search(ar,space,left - 1,right - 1,x) << "\n";
+        cout << "The index is:" << ternary_search(ar, space, left - 1, right - 1, x) << "\n";
     }
     return 0;
 }
