553390ea5937385d70abe4cd5d02214ae336dcd7
TUSHARZ <tusharrrrr97@gmail.com>
2017-10-17 21:17:19 +0530

    added_linked_list_in_ruby

---
+++ linked_list/linked_list.rb
@@ -0,0 +1,49 @@
+# Basic Node
+class Node
+  attr_accessor :val, :next
+
+  def initialize(val, next_node)
+      @val = val
+      @next = next_node
+  end
+end
+
+# Linked List Implementation
+class LinkedList
+
+  def initialize(val)
+    @head = Node.new(val, nil)
+  end
+
+  def add(val)
+    current = @head
+    while current.next != nil
+      current = current.next
+    end
+    current.next = Node.new(val, nil)
+  end
+
+  def delete(val)
+    current.next = @head
+    if current.val = val
+      @head = current.next
+    else
+      while (current.next != nil) && (current.next.val != val)
+        current = current.next
+      end
+      unless current.next == nil
+        current.next = current.next.next
+      end
+    end
+  end
+
+  def return_list
+    elements = []
+    current = @head
+    while current.next != nil
+      elements << current
+      current = current.next
+    end
+    elements << current
+  end
+end
