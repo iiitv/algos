670f73d93ac68c2231242b5c7f96d7833eadd889
Chris <cjmang@gmail.com>
2017-10-21 11:54:33 -0500

    Readding Rotate and ToString

--- stack/Stack.cs
+++ stack/Stack.cs
@@ -15,10 +15,19 @@ class MainClass
         int max_size = 10;
         for (int i = 1; i <= max_size; i++)
         {
-            string ele = String.Format("{0} duck", i);
+            string ele = String.Format("{0} duck", max_size + 1 - i);
             Console.WriteLine("Pushing {0} to the stack", ele);
             test.Push(ele);
         }
+
+        Console.WriteLine("Currently in the stack: %n {0}", test.ToString());
+
+        Console.WriteLine("Rotating");
+        test.Rotate(4, true);
+        Console.WriteLine(test.ToString());
+
+        test.Rotate(5, false);
+        Console.WriteLine(test.ToString());
 
         Console.WriteLine("Popping off the stack.");
         for (int i = 1; i <= max_size; i++)
@@ -52,6 +61,66 @@ class Stack<T>
         T t = stack.Last();
         stack.RemoveLast();
         return t;
+    }
+
+    public Boolean isEmpty()
+    {
+        return !stack.Any();
+    }
+
+    /*
+     * Gets the top n elements and rotates them
+     *
+     * If left == false then it will move the last element to behind the nth element (from the top)
+     *
+     * If left == true it will move the nth element(from the top) to the top
+     */
+    public void Rotate(int n, bool left)
+    {
+        Stack<T> tmp = new Stack<T>();
+        T ele;
+        if (left)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                tmp.Push(this.Pop());
+            }
+            ele = tmp.Pop();
+        }
+        else
+        {
+            ele = this.Pop();
+            for (int i = 0; i < n - 1; i++)
+            {
+                tmp.Push(this.Pop());
+            }
+            tmp.Push(ele);
+        }
+        while(!tmp.isEmpty())
+        {
+            this.Push(tmp.Pop());
+        }
+        if(left)
+        {
+            this.Push(ele);
+        }
+    }
+
+    public override string ToString()
+    {
+        Stack<T> tmp = new Stack<T>();
+        string s = "Stack: ";
+        while (!this.isEmpty())
+        {
+            T ele = this.Pop();
+            s += String.Format("{0} | ", ele.ToString());
+            tmp.Push(ele);
+        }
+        while (!tmp.isEmpty())
+        {
+            this.Push(tmp.Pop());
+        }
+        return s;
     }
 }
 
