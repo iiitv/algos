3424f1607704ec60baa52fe1ff501b2d8af6b0b2
amineHorseman <bendahmane.amine@gmail.com>
2017-10-24 21:54:59 +0100

    Add iterative approach for Depth First Traversal [python]

--- depth_first_traversal/DepthFirstTraversal.py
+++ depth_first_traversal/DepthFirstTraversal.py
@@ -1,10 +1,29 @@
-def depthFirstTraversal(graph, node, visited):
+def depthFirstTraversalRecursive(graph, node, visited):
     if node not in visited:
         visited.append(node)
         childs = graph[node]
         if len(childs) > 0:
             for child in childs:
-                depthFirstTraversal(graph, child, visited)
+                depthFirstTraversalRecursive(graph, child, visited)
+    return visited
+
+
+def depthFirstTraversalIterative(graph, node):
+    visited = []
+    stack = []
+    stack.append(node)
+    while len(stack) > 0:
+        node = stack[-1]
+        if node not in visited:
+            visited.append(node)
+            childs = graph[node]
+            while len(childs) > 0:
+                # add the non-visited childs to the stack in reverse order
+                child = childs.pop()
+                if child not in visited:
+                    stack.append(child)
+        else:
+            stack.pop()
     return visited
 
 
@@ -16,8 +35,15 @@ def main():
              'E': ['F'],
              'F': ['C'],
              'G': []}
-    result = depthFirstTraversal(graph, 'A', [])
+
+    print "Recusrive:"
+    result = depthFirstTraversalRecursive(graph, 'A', [])
     for node in result:
+        print(node)
+
+    print "Iterative:"
+    result2 = depthFirstTraversalIterative(graph, 'A')
+    for node in result2:
         print(node)
 
 
