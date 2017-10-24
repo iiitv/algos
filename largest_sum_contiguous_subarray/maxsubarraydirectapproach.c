34bff26d020e18c6cbe393e31d889c2c93dd773b
Pooja Bharadwaj <pooja7b@users.noreply.github.com>
2017-10-24 12:26:48 +0530

    Update maxsubarraydirectapproach.c

--- largest_sum_contiguous_subarray/maxsubarraydirectapproach.c
+++ largest_sum_contiguous_subarray/maxsubarraydirectapproach.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
 int maxsum(int arr[], int arrlen)
 {
 	int sum=0,i,j,k,max=-32767;
@@ -20,7 +19,6 @@ int maxsum(int arr[], int arrlen)
 	}
 	return max;
 }
-
 int main(int argc, char* argv[]) //taking the array as command line argument
 {
 	int i,m, j=0, pos=0;
@@ -37,26 +35,27 @@ int main(int argc, char* argv[]) //taking the array as command line argument
 	for(i=0;i<k;i++)
 	{
 		if(n[i]!=' ')
-    {
-      while(n[i]!=' ' && i<k)
-      {
-        val[j]=n[i];
-        j++;
-        i++;
-      }
-      if(val[0]=='-')
-        {
-          a[pos]=-atoi(val+1); pos++;
-        }
-      else { 
-        a[pos]=atoi(val); pos++;
-        }
-      for(m=0;m<7;m++)
-      {
-        val[m]=' ';
-      }
-      j=0;
-	  }
+    	{
+			while(n[i]!=' ' && i<k)
+			{
+				val[j]=n[i];
+				j++;
+				i++;
+			}
+			if(val[0]=='-')
+			{
+				a[pos]=-atoi(val+1); pos++;
+			}
+			else 
+			{ 
+				a[pos]=atoi(val); pos++;
+			}
+			for(m=0;m<7;m++)
+			{
+				val[m]=' ';
+			}
+			j=0;
+	  	}
 	}
 	printf("%d",maxsum(a,pos));
 	return 0;
