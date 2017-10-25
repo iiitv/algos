4fee459d13bf1ae5ac1d9cf3a6a0fb4804b82c26
Luci <rohella.anshuman@gmail.com>
2017-10-26 00:38:05 +0900

    Updated permute.py for whitespaces

--- string_permutation_problem/permute.py
+++ string_permutation_problem/permute.py
@@ -1,25 +1,26 @@
 def permute(s):
     out = []
     
-    # Base Case
+    #Base Case
     if len(s) == 1:
         out = [s]
         
     else:
-        # For every letter in string
+        #For every letter in String
         for i, let in enumerate(s):
             
-            # For every permutation resulting from Step 2 and 3 described above
+            #for every permutation resulting from Step 2 and 3 described above
+            
             for perm in permute(s[:i] + s[i+1:]):
                 
-                # Add it to output
+                #Add it to output
                 out += [let + perm]
-
+                
     return out
-   
+
 def main():
+    
     permute('abc')
-
-
+    
 if __name__ == '__main__':
     main()
