2512942b6c4087508592c45b64e8611713fffd6c
Luci <rohella.anshuman@gmail.com>
2017-10-26 00:31:27 +0900

    Add String Permutation [Python]

---
+++ string_permutation_problem/permute.py
@@ -0,0 +1,25 @@
+def permute(s):
+    out = []
+    
+    # Base Case
+    if len(s) == 1:
+        out = [s]
+        
+    else:
+        # For every letter in string
+        for i, let in enumerate(s):
+            
+            # For every permutation resulting from Step 2 and 3 described above
+            for perm in permute(s[:i] + s[i+1:]):
+                
+                # Add it to output
+                out += [let + perm]
+
+    return out
+   
+def main():
+    permute('abc')
+
+
+if __name__ == '__main__':
+    main()
