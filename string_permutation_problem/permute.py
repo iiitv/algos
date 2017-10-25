137935ebaefa1b334a8541b3b2f33ea96f198c5c
Luci <rohella.anshuman@gmail.com>
2017-10-26 01:05:57 +0900

    Updated print and spacing

--- string_permutation_problem/permute.py
+++ string_permutation_problem/permute.py
@@ -4,20 +4,22 @@ def permute_my_string(str):
         This is a recursive solution to the string permutation problem.
         The user is supposed to return all the permutations for a given string.
     """
-    #Base Case
+    # Base Case
     if len(str) == 1:
         output_string = [str]
     else:
-        #For every letter in String
+        # For every letter in String
         for i, letter in enumerate(str):
-            #for every permutation resulting from Step 2 and 3 described above
+            # for every permutation resulting from Step 2 and 3 described above
             for perm in permute(str[:i] + str[i+1:]):
-                #Add it to output
+                # Add it to output
                 output_string += [letter + perm]
     return output_string
 
+
 def main():
-    permute_my_string('abc')
-    
+    print(permute_my_string('abc'))
+
+
 if __name__ == '__main__':
     main()
