1987d90ec12ea0cb44d2c3173a21371bfa02480c
Luci <rohella.anshuman@gmail.com>
2017-10-26 13:54:24 +0900

    Add permute.java

---
+++ string_permutation_problem/permute.java
@@ -0,0 +1,25 @@
+public class permute{
+
+    /* A recursive solution to a classic problem to print all the permutations 
+      in a string. */
+
+public static void permutation(String str) { 
+    permutation("", str); 
+}
+
+private static void permutation(String prefix, String str) {
+    int n = str.length();
+    if (n == 0) System.out.println(prefix);
+    else {
+        for (int i = 0; i < n; i++)
+            permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
+    }
+}
+
+  
+ public static void main(String[] args) {
+        permutation("abc");
+    		
+	 }
+
+}
