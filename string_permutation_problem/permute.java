b559d357b76707bfbf40df7c3be9e448d36c7bc5
Luci <rohella.anshuman@gmail.com>
2017-10-26 14:36:33 +0900

    updates spaces

--- string_permutation_problem/permute.java
+++ string_permutation_problem/permute.java
@@ -1,25 +1,23 @@
 public class Permute{
 
-    /* A recursive solution to a classic problem to print all the permutations 
+    /* A recursive solution to a classic problem to print all the permutations
       in a string. */
 
-public static void permutation(String str) { 
-    permutation("", str); 
-}
+    public static void permutation(String str) {
+        permutation("", str);
+        }
 
-private static void permutation(String prefix, String str) {
-    int n = str.length();
-    if (n == 0) System.out.println(prefix);
-    else {
-        for (int i = 0; i < n; i++)
-            permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
-    }
-}
+    private static void permutation(String prefix, String str) {
+        int n = str.length();
+        if (n == 0) System.out.println(prefix);
+        else {
+            for (int i = 0; i < n; i++)
+                permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
+            }
+        }
 
-  
  public static void main(String[] args) {
         permutation("abc");
-    		
-	 }
+        }
 
 }
