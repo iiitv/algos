af6182e90cc09824b3d179e8c86f2e0afa66fb00
LoganDungeon <JonasW96@gmx.net>
2017-10-28 15:33:13 +0200

    Few changes for more code quality and added some comments

--- bubble_sort/BubbleSort.java
+++ bubble_sort/BubbleSort.java
@@ -1,14 +1,19 @@
 public class BubbleSort {
 
-    private static void sort(int[] list){
-        if(list == null || list.length == 0 || list.length == 1)
+    private static void sort(int[] list) {
+        // if the list is null or has zero/one element, we are ready
+        if (list == null || list.length == 0 || list.length == 1) {
             return;
+        }
         boolean swapped;
         int j = 1;
-        do{
+        // for every iteration, go through the whole list and check if it is sorted
+        do {
             swapped = false;
+            // for every iteration we know for sure, that the last number ist sorted, so we can ignore them (j)
             for (int i = 0; i < list.length - j; i++) {
-                if(list[i] > list[i+1]){
+                // if the numbers are not sorted, swap them and set swapped to true, to go through the list again.
+                if (list[i] > list[i+1]) {
                     int temp = list[i];
                     list[i] = list[i+1];
                     list[i+1] = temp;
@@ -16,12 +21,11 @@ public class BubbleSort {
                 }
             }
             j++;
-        }
-        while (swapped);
+        } while (swapped);
     }
 
-    public static void main(String[] args){
-        int[] list = { 8, 4, 6, 1, 10, 7, 2, 5, 9, 3 };
+    public static void main(String[] args) {
+        int[] list = {8, 4, 6, 1, 10, 7, 2, 5, 9, 3};
         sort(list);
         for (int a : list) {
             System.out.print(a + " ");
