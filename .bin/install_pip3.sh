dd1584e4e5e0b4a7c671135cc05029db915eada5
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2018-07-01 22:27:50 +0530

    change pip3 to python3 -m pip
    `pip3` ==> `python3 -m pip`

--- .bin/install_pip3.sh
+++ .bin/install_pip3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo -H pip3 install --upgrade pip
-sudo -H pip3 install -Ur pip3-requirements.txt
+sudo -H python3 -m pip install --upgrade pip
+sudo -H python3 -m pip install -Ur pip3-requirements.txt
