764e6ae334e2c0a4f6cc6fc73fc50987425dfb4a
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2018-07-01 22:32:16 +0530

    ignore pip3 installed packages

--- .bin/install_pip3.sh
+++ .bin/install_pip3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo -H python3 -m pip install --upgrade pip
-sudo -H python3 -m pip install -Ur pip3-requirements.txt
+sudo -H python3 -m pip install --ignore-installed -Ur pip3-requirements.txt
