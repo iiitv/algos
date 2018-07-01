f5843a7f2f6156adce4d7a80e43b2dd288411c45
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2018-07-01 23:03:07 +0530

    Update install_go.sh

--- .bin/install_go.sh
+++ .bin/install_go.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir -p $GOPATH/src/golang.org/golang/lint \
+mkdir -p $GOPATH/src/golang.org/golang \
   && git clone https://github.com/golang/lint.git $GOPATH/src/golang.org/golang/lint \
-  && go get -u golang.org/golang/lint
+  && go get -u golang.org/golang/lint/golang
 cd $GOPATH/src/github.com/golang/lint
 go install .
