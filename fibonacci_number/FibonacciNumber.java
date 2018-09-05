b90fa523491e1919d1fb142af783a7dcf71542ba
shikherjaiswal <2015csb1116@iitrpr.ac.in>
2018-09-05 18:33:05 +0530

    Fixed base case for n=2

--- fibonacci_number/FibonacciNumber.java
+++ fibonacci_number/FibonacciNumber.java
@@ -1,27 +1,31 @@
 public class FibonacciNumber {
 
     public static void fibonacci(int n) {
-      if(n <= 1) {
-        System.out.println(n);
-        return ;
+      if(n == 1) {
+        System.out.println("0");
       }
-      int a = 0;     // Store second last fibonacci number
-      int b = 1;    // Store last fibonacci number
-      int fib = 0;  // Store current fibonacci number which is sum of last and second last fibonacci no
-      for(int i = 2; i < n; i++) {
-        fib = a + b;
-        a = b;
-        b = fib;
+      else if(n == 2) {
+        System.out.println("1");
       }
-      System.out.println(fib);
+      else {
+        int a = 0;     // Store second last fibonacci number
+        int b = 1;    // Store last fibonacci number
+        int fib = 0;  // Store current fibonacci number which is sum of last and second last fibonacci no
+        for(int i = 2; i < n; i++) {
+          fib = a + b;
+          a = b;
+          b = fib;
+        }
+        System.out.println(fib);
+      }
     }
 
-    public static void main(String[] args) {
-        int n = 5;           // n>=0
-        if(n < 0) {
-          System.out.println("n cannot be less than 0");
-          return ;
-        }
-        fibonacci(n);
+      public static void main(String[] args) {
+          int n = 5;           // n>=0
+          if(n < 0) {
+            System.out.println("n cannot be less than 0");
+            return ;
+          }
+          fibonacci(n);
     }
 }
