6067a9de4fb6fcc86c85fd371cccf860648551b9
shikherjaiswal <2015csb1116@iitrpr.ac.in>
2018-09-07 21:12:02 +0530

    added alternative code for base case and fixed indentation

--- fibonacci_number/FibonacciNumber.java
+++ fibonacci_number/FibonacciNumber.java
@@ -1,31 +1,32 @@
 public class FibonacciNumber {
 
-    public static void fibonacci(int n) {
-      if(n == 1) {
-        System.out.println("0");
-      }
-      else if(n == 2) {
-        System.out.println("1");
-      }
-      else {
-        int a = 0;     // Store second last fibonacci number
-        int b = 1;    // Store last fibonacci number
-        int fib = 0;  // Store current fibonacci number which is sum of last and second last fibonacci no
-        for(int i = 2; i < n; i++) {
-          fib = a + b;
-          a = b;
-          b = fib;
+    // fibonacci function return the nth fibonacci no for all n>0
+    public static int fibonacci(int n) {
+        if (n == 1 || n == 2) {
+            return (n - 1);
+        } else {
+            // Store second last fibonacci number
+            int a = 0;
+            // Store last fibonacci number
+            int b = 1;
+            // Store current fibonacci number which is sum of last and second last fibonacci no
+            int fib = 0;
+            for (int i = 2; i < n; i++) {
+                fib = a + b;
+                a = b;
+                b = fib;
+            }
+            return (fib);
         }
-        System.out.println(fib);
-      }
     }
 
     public static void main(String[] args) {
-      int n = 5;           // n>=0
-      if(n < 0) {
-        System.out.println("n cannot be less than 0");
-        return ;
-      }
-      fibonacci(n);
+        // n>0
+        int n = 5;
+        if (n <= 0) {
+            System.out.println("n must be greater than 0");
+            return;
+        }
+        System.out.println(fibonacci(n));
     }
 }
