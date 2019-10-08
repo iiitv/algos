70c98cee008f363252b155071c5a5963ae3692f5
Jonathan Gómez <jonicgp97@gmail.com>
2019-10-08 16:42:43 -0500

    Update fibonacci.js

--- fibonacci_number/fibonacci.js
+++ fibonacci_number/fibonacci.js
@@ -1,19 +1,22 @@
-function fibonacci(num, memo) {
-  memo = memo || {1:0, 2:1};
+function fibonacci (num, memo) {
+	memo = memo || { 1:0, 2:1 };
 
-  if (memo[num] || memo[num] === 0)
-    return memo[num];
+	if (memo[num] || memo[num] === 0) {
+		return memo[num];
+	}
 
-  return memo[num] = fibonacci(num - 1, memo) + fibonacci(num - 2, memo);
+	memo[num] = fibonacci(num - 1, memo) + fibonacci(num - 2, memo);
+
+	return memo[num];
 }
 
-function main() {
-  console.log("result => " + fibonacci(5));
-  // result => 3
-  console.log("result => " + fibonacci(12));
-  // result => 89
-  console.log("result => " + fibonacci(50));
-  // result => 7778742049
+function main () {
+	console.log('result => ' + fibonacci(5));
+	// result => 3
+	console.log('result => '  + fibonacci(45));
+	// result => 89
+	console.log('result => ' + fibonacci(50));
+	// result => 7778742049
 }
 
 main();
