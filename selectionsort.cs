60f947dbefe28b2a1ca72110ac82834134ebe63f
Amasha007 <sewminiamasha77@gmail.com>
2019-10-23 11:17:46 +0530

    added selection sort

---
+++ selectionsort.cs
@@ -0,0 +1,44 @@
+// C# program for implementation  
+// of Selection Sort 
+using System; 
+  
+class GFG 
+{  
+    static void sort(int []arr) 
+    { 
+        int n = arr.Length; 
+  
+        // One by one move boundary of unsorted subarray 
+        for (int i = 0; i < n - 1; i++) 
+        { 
+            // Find the minimum element in unsorted array 
+            int min_idx = i; 
+            for (int j = i + 1; j < n; j++) 
+                if (arr[j] < arr[min_idx]) 
+                    min_idx = j; 
+  
+            // Swap the found minimum element with the first 
+            // element 
+            int temp = arr[min_idx]; 
+            arr[min_idx] = arr[i]; 
+            arr[i] = temp; 
+        } 
+    } 
+  
+    // Prints the array 
+    static void printArray(int []arr) 
+    { 
+        int n = arr.Length; 
+        for (int i=0; i<n; ++i) 
+            Console.Write(arr[i]+" "); 
+        Console.WriteLine(); 
+    } 
+  
+    // Driver code  
+    public static void Main() 
+    { 
+        int []arr = {64,25,12,22,11}; 
+        sort(arr); 
+        Console.WriteLine("Sorted array"); 
+        printArray(arr); 
+    } 
