1c94c57ee1f880f5aa37a62ab0b797666d353680
himanshusanecha <52252631+himanshusanecha@users.noreply.github.com>
2020-08-29 21:43:14 +0530

    Updated BreadthFirstSearch.c
    there were some issues with the code proper algorithm was not being implemented due to minor mistakes

--- breadth_first_search/BreadthFirstSearch.c
+++ breadth_first_search/BreadthFirstSearch.c
@@ -2,83 +2,131 @@
 vertex are traversed. And this process goes on.
 This process is similar to level order traversal of trees */
 
-#include <stdio.h>
-#include <stdlib.h>
-int N; // no of vertices in a graph
-int adj[100][100]={0}; // adjacency matrix
-int state[100]; //state of a node whether it is at initial, waiting or at visited stage
-void BFS_traversal(); 
-void BFS(int a);
-int queue[100];
-int front = -1,rear = -1;
-void insert(int vertex);
+
+#include<stdio.h>
+#include<stdlib.h>
+ 
+#define MAX 100  
+ 
+#define initial 1
+#define waiting 2
+#define visited 3
+ 
+int n;    
+int adj[MAX][MAX];
+int state[MAX]; 
+void create_graph();
+void BF_Traversal();
+void BFS(int v);
+ 
+int queue[MAX], front = -1,rear = -1;
+void insert_queue(int vertex);
 int delete_queue();
-int isempty();
-void BFS_traversal(){
+int isEmpty_queue();
+ 
+int main()
+{
+	create_graph();
+	BF_Traversal();
+	return 0;
+}
+ 
+void BF_Traversal()
+{
 	int v;
-	for(v=0;v<N;v++)
-		state[v] = 1;
-	printf("Enter starting vertex for breadth first search : ");
-	scanf("%d",&v);
+	
+	for(v=0; v<n; v++) 
+		state[v] = initial;
+	
+	printf("Enter Start Vertex for BFS: \n");
+	scanf("%d", &v);
 	BFS(v);
 }
-void BFS(int v){
+ 
+void BFS(int v)
+{
 	int i;
-	insert(v);
-	state[v]=2;
-	while(!isempty())
+	
+	insert_queue(v);
+	state[v] = waiting;
+	
+	while(!isEmpty_queue())
 	{
-		v = delete_queue();
+		v = delete_queue( );
 		printf("%d ",v);
-		state[v]=3;
-		for(i=0;i<N;i++)
+		state[v] = visited;
+		
+		for(i=0; i<n; i++)
 		{
-			if(adj[v][i]==1 && state[i]==1)
+			if(adj[v][i] == 1 && state[i] == initial) 
 			{
-				insert(i);
-				state[i]=2;
+				insert_queue(i);
+				state[i] = waiting;
 			}
 		}
-		printf("\n");
 	}
+	printf("\n");
 }
-void insert(int vertex){
-	if(rear==100-1)
-		printf("Queue overflow\n");
+ 
+void insert_queue(int vertex)
+{
+	if(rear == MAX-1)
+		printf("Queue Overflow\n");
 	else
 	{
-		if(front==-1)
-			front=0;
-		rear = rear + 1;
-		queue[rear] = vertex;
+		if(front == -1) 
+			front = 0;
+		rear = rear+1;
+		queue[rear] = vertex ;
 	}
 }
-int isempty(){
+ 
+int isEmpty_queue()
+{
 	if(front == -1 || front > rear)
 		return 1;
 	else
 		return 0;
 }
-int delete_queue(){
-	int deleteitem;
-	if(front==-1 || front>rear)
+ 
+int delete_queue()
+{
+	int delete_item;
+	if(front == -1 || front > rear)
 	{
-		printf("Queue underflow\n");
+		printf("Queue Underflow\n");
 		exit(1);
 	}
-	deleteitem = queue[front];
-	front = front - 1;
-	return deleteitem;
+	
+	delete_item = queue[front];
+	front = front+1;
+	return delete_item;
 }
-int main()
+ 
+void create_graph()
 {
-	N = 4;
-	adj[0][1]=1;
-	adj[0][3]=1;
-	adj[1][0]=1;
-	adj[1][2]=1;
-	adj[1][3]=1;
-	adj[2][3]=1;
-	adj[3][1]=1;
-	BFS_traversal();
+	int count,max_edge,origin,destin;
+ 
+	printf("Enter number of vertices : ");
+	scanf("%d",&n);
+	max_edge = n*(n-1);
+ 
+	for(count=1; count<=max_edge; count++)
+	{
+		printf("Enter edge %d( -1 -1 to quit ) : ",count);
+		scanf("%d %d",&origin,&destin);
+ 
+		if((origin == -1) && (destin == -1))
+			break;
+ 
+		if(origin>=n || destin>=n || origin<0 || destin<0)
+		{
+			printf("Invalid edge!\n");
+			count--;
+		}
+		else
+		{
+			adj[origin][destin] = 1;
+		}
+	}
 }
