e143c7a3a4f21df580b09e32d2c0ac220f4a5540
himanshusanecha <52252631+himanshusanecha@users.noreply.github.com>
2020-08-30 13:59:01 +0530

    Applied indentation

--- breadth_first_search/BreadthFirstSearch.c
+++ breadth_first_search/BreadthFirstSearch.c
@@ -5,20 +5,16 @@ This process is similar to level order traversal of trees */
 
 #include<stdio.h>
 #include<stdlib.h>
- 
 #define MAX 100  
- 
 #define initial 1
 #define waiting 2
 #define visited 3
- 
 int n;    
 int adj[MAX][MAX];
 int state[MAX]; 
 void create_graph();
 void BF_Traversal();
 void BFS(int v);
- 
 int queue[MAX], front = -1,rear = -1;
 void insert_queue(int vertex);
 int delete_queue();
@@ -30,32 +26,25 @@ int main()
 	BF_Traversal();
 	return 0;
 }
- 
 void BF_Traversal()
 {
 	int v;
-	
 	for(v=0; v<n; v++) 
 		state[v] = initial;
-	
 	printf("Enter Start Vertex for BFS: \n");
 	scanf("%d", &v);
 	BFS(v);
 }
- 
 void BFS(int v)
 {
 	int i;
-	
 	insert_queue(v);
 	state[v] = waiting;
-	
 	while(!isEmpty_queue())
 	{
 		v = delete_queue( );
 		printf("%d ",v);
-		state[v] = visited;
-		
+		state[v] = visited;	
 		for(i=0; i<n; i++)
 		{
 			if(adj[v][i] == 1 && state[i] == initial) 
@@ -66,8 +55,7 @@ void BFS(int v)
 		}
 	}
 	printf("\n");
-}
- 
+} 
 void insert_queue(int vertex)
 {
 	if(rear == MAX-1)
@@ -80,7 +68,6 @@ void insert_queue(int vertex)
 		queue[rear] = vertex ;
 	}
 }
- 
 int isEmpty_queue()
 {
 	if(front == -1 || front > rear)
@@ -88,7 +75,6 @@ int isEmpty_queue()
 	else
 		return 0;
 }
- 
 int delete_queue()
 {
 	int delete_item;
@@ -97,28 +83,22 @@ int delete_queue()
 		printf("Queue Underflow\n");
 		exit(1);
 	}
-	
 	delete_item = queue[front];
 	front = front+1;
 	return delete_item;
 }
- 
 void create_graph()
 {
 	int count,max_edge,origin,destin;
- 
 	printf("Enter number of vertices : ");
 	scanf("%d",&n);
 	max_edge = n*(n-1);
- 
 	for(count=1; count<=max_edge; count++)
 	{
 		printf("Enter edge %d( -1 -1 to quit ) : ",count);
 		scanf("%d %d",&origin,&destin);
- 
 		if((origin == -1) && (destin == -1))
 			break;
- 
 		if(origin>=n || destin>=n || origin<0 || destin<0)
 		{
 			printf("Invalid edge!\n");
