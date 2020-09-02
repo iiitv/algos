5372898dddf514f5d2f2b5fa3dcf06100acbe674
Creator5923 <nlokhande5923@gmail.com>
2020-09-01 22:49:16 -0700

    minute changes updated comments

--- insertion_sort/insertion_sort.cpp
+++ insertion_sort/insertion_sort.cpp
@@ -6,19 +6,19 @@ void insertionSort(int array[], int size)
 	int i = 0, j = 0, tempElement = 0, tempIndex = 0, flag = 0;
 	for (i = 1; i < size; i++)
 	{
-		tempElement = array[i];
+		tempElement = array[i]; //taking an array element into tempElement
 		for (j = i - 1; j >= 0; j--)
 		{
 			if (array[j] > tempElement)
 				array[j + 1] = array[j];
 			else
-				flag = 1;
+				flag = 1; //checking if we got the correct position of tempElement
 			break;
 		}
-		if (flag == 1 || j == -1)
+		if (flag == 1 || j == -1) //condition checking if we got correct position of tempElement or not
 		{
 			flag = 0;
-			array[j + 1] = tempElement;
+			array[j + 1] = tempElement; //putting tempElement to it correct position
 		}
 	}
 }
@@ -36,8 +36,8 @@ int main()
 	int array[] = {5, 4, 233, 32, 1, 4, 34, 3, 23};
 	int size = sizeof(array) / sizeof(array[0]);
 	cout << "unsorted array is :" << endl;
-	display(array, size);
+	display(array, size); //display unsorted array
 	insertionSort(array, size);
 	cout << "sorted array is :" << endl;
-	display(array, size);
+	display(array, size); //display the sorted array
 }
