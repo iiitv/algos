aecc310a93600d1f1a35cbd0dadc829d33dc9b00
Creator5923 <nlokhande5923@gmail.com>
2020-09-01 22:38:48 -0700

    minute changes removed comments

--- insertion_sort/insertion_sort.cpp
+++ insertion_sort/insertion_sort.cpp
@@ -1,40 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 void insertionSort(int array[], int size)
 {
 	int i = 0, j = 0, tempElement = 0, tempIndex = 0, flag = 0;
 	for (i = 1; i < size; i++)
 	{
-		tempElement = array[i]; // taking an array element into tempElement
+		tempElement = array[i];
 		for (j = i - 1; j >= 0; j--)
 		{
 			if (array[j] > tempElement)
 				array[j + 1] = array[j];
 			else
-				flag = 1; //checking if we got the correct position of tempElement
+				flag = 1;
 			break;
 		}
-		if (flag == 1 || j == -1) //condition checking if we got correct position of tempElement or not
+		if (flag == 1 || j == -1)
 		{
 			flag = 0;
-			array[j + 1] = tempElement; //putting tempElement to it correct position
+			array[j + 1] = tempElement;
 		}
 	}
 }
+
 void display(int array[], int size)
 {
 	int i = 0;
 	for (i = 0; i < size; i++)
-		cout << array[i] << " "; //printing array
+		cout << array[i] << " ";
 	cout << endl;
 }
+
 int main()
 {
 	int array[] = {5, 4, 233, 32, 1, 4, 34, 3, 23};
 	int size = sizeof(array) / sizeof(array[0]);
 	cout << "unsorted array is :" << endl;
-	display(array, size);		//display unsorted array
-	insertionSort(array, size); //calling insertion sort
+	display(array, size);
+	insertionSort(array, size);
 	cout << "sorted array is :" << endl;
-	display(array, size); //display the sorted array
+	display(array, size);
 }
