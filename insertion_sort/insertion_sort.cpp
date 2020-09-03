128d3a473c0a7418613c626a95fffa869ccaa7fc
Creator5923 <nlokhande5923@gmail.com>
2020-09-03 09:59:50 -0700

    code modified a bit

--- insertion_sort/insertion_sort.cpp
+++ insertion_sort/insertion_sort.cpp
@@ -1,25 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 void insertionSort(int array[], int size)
 {
-	int i = 0, j = 0, tempElement = 0, tempIndex = 0, flag = 0;
+	int i = 0, tempElement = 0, j = 0;
 	for (i = 1; i < size; i++)
 	{
-		tempElement = array[i]; //taking an array element into tempElement
-		for (j = i - 1; j >= 0; j--)
+		tempElement = array[i]; //Choosing element whose correct index to be found.
+		j = i - 1;
+		while (j >= 0 && array[j] > tempElement)
 		{
-			if (array[j] > tempElement)
-				array[j + 1] = array[j];
-			else
-				flag = 1; //checking if we got the correct position of tempElement
-			break;
+			array[j + 1] = array[j]; //Shifting element one position right side untill we get correct position
+			j = j - 1;
 		}
-		if (flag == 1 || j == -1) //condition checking if we got correct position of tempElement or not
-		{
-			flag = 0;
-			array[j + 1] = tempElement; //putting tempElement to it correct position
-		}
+		array[j + 1] = tempElement; // got the correct position for tempElement
 	}
 }
 void display(int array[], int size)
