a7969b05854372966e6e07a4ace20058bf9e9841
Adit Alware <42518974+aditalware@users.noreply.github.com>
2020-09-18 23:37:56 +0530

    Update FibonacciNumber.js

--- fibonacci_number/FibonacciNumber.js
+++ fibonacci_number/FibonacciNumber.js
@@ -1,9 +1,13 @@
-
+/*fibonacci function is going to take a number "num"
+and going to show the "num" th Fibonacci number on the console*/
 function fibonacci(num){
+	/*declaring an array*/
 var arr=[];
+	/*array's num+1 th position will contain fibonacci number at num th position*/
 arr.length=num+1;
 for(let i=1;i<arr.length;i++)
 {
+	/* Base case*/
   if(i===1 ||i===2)
   {
    arr[i]=1;
@@ -16,3 +20,5 @@ console.log(arr[num]);
 }
 
 fibonacci(20);
+
+Output: 6765
