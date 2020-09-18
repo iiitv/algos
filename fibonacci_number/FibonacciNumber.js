7fa1747329d15e8f144cc588d80b89007503519c
Adit Alware <42518974+aditalware@users.noreply.github.com>
2020-09-18 23:33:31 +0530

    Update FibonacciNumber.js

--- fibonacci_number/FibonacciNumber.js
+++ fibonacci_number/FibonacciNumber.js
@@ -1,1 +1,18 @@
 
+function fibonacci(num){
+var arr=[];
+arr.length=num+1;
+for(let i=1;i<arr.length;i++)
+{
+  if(i===1 ||i===2)
+  {
+   arr[i]=1;
+  }
+  else{
+    arr[i]=arr[i-1]+arr[i-2];
+  }
+}
+console.log(arr[num]);
+}
+
+fibonacci(20);
