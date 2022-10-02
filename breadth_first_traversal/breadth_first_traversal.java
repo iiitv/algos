e8869210c68ae7dd22c2050475f43eb8a0102f9b
Satyam Tripathi <98680993+satyacasm@users.noreply.github.com>
2022-10-02 13:07:51 +0530

    Added breadth_first_traversal.java

---
+++ breadth_first_traversal/breadth_first_traversal.java
@@ -0,0 +1,75 @@
+import java.util.*;
+
+class breadth_first_traversal {
+    
+    
+    public static void main(String[] args) {
+        Graph g=new Graph(6);
+        g.addEdge(0, 1);
+        g.addEdge(0, 5);
+        g.addEdge(1, 2);
+        g.addEdge(1, 4);
+        g.addEdge(5, 2);
+        g.addEdge(5, 4);
+        g.addEdge(4, 3);
+        g.addEdge(2, 3);
+        g.BFS(0);
+
+    }
+}
+class Graph
+{
+    int V;   // No. of vertices
+    LinkedList<Integer> adj[]; //Adjacency Lists
+ 
+    // Constructor
+    Graph(int v)
+    {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i=0; i<v; ++i)
+            adj[i] = new LinkedList();
+    }
+ 
+    // Function to add an edge into the graph
+    void addEdge(int v,int w)
+    {
+        adj[v].add(w);
+    }
+ 
+    // prints BFS traversal from s
+    void BFS(int s)
+    {
+        // Mark all the vertices as not visited(By default
+        // set as false)
+        boolean visited[] = new boolean[V];
+ 
+        // Creates a queue for BFS
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+ 
+        // Mark the current node as visited and enqueue it
+        visited[s]=true;
+        queue.add(s);
+ 
+        while (queue.size() != 0)
+        {
+            // Dequeue a vertex from queue and print it
+            s = queue.poll();
+            System.out.print(s+" ");
+ 
+            // Get all adjacent vertices of the dequeued vertex s
+            // If a adjacent has not been visited, then mark it
+            // visited and enqueue it
+            Iterator<Integer> i = adj[s].listIterator();
+            while (i.hasNext())
+            {
+                int n = i.next();
+                if (!visited[n])
+                {
+                    visited[n] = true;
+                    queue.add(n);
+                }
+            }
+        }
+    }
+}
