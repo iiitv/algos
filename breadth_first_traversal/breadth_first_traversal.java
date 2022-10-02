37237e52dcb0ee89ad2d7803042828fd33a856ec
Satyam Tripathi <98680993+satyacasm@users.noreply.github.com>
2022-10-02 14:18:28 +0530

    Update breadth_first_traversal.java

--- breadth_first_traversal/breadth_first_traversal.java
+++ breadth_first_traversal/breadth_first_traversal.java
@@ -4,16 +4,19 @@ class breadth_first_traversal {
     
     
     public static void main(String[] args) {
-        Graph g=new Graph(6);
-        g.addEdge(0, 1);
-        g.addEdge(0, 5);
+        Graph g=new Graph(7);
+
+        //1 Based indexing 
         g.addEdge(1, 2);
-        g.addEdge(1, 4);
-        g.addEdge(5, 2);
-        g.addEdge(5, 4);
-        g.addEdge(4, 3);
+        g.addEdge(1, 6);
         g.addEdge(2, 3);
-        g.BFS(0);
+        g.addEdge(2, 5);
+        g.addEdge(6, 3);
+        g.addEdge(6, 5);
+        g.addEdge(5, 4);
+        g.addEdge(3, 4);
+        System.out.println("BFS Traversal of the given graph is -");
+        g.BFS(1);
 
     }
 }
