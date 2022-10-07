0f8d9e53f93c930f7fca80416d5d56aa2e8d8437
Varada M <58105997+varada1110@users.noreply.github.com>
2022-10-07 16:14:27 +0530

    Added FibonacciNumber.py

--- fibonacci_number/FibonacciNumber.py
+++ fibonacci_number/FibonacciNumber.py
@@ -4,13 +4,17 @@ def fib(n):
     else:
         return fib(n-1) + fib(n-2)
 
-print("Enter the number :")
-t = int(input())
-if(t == 0):
-    print("Enter correct number!")
-else:
-        
-    for i in range(t):
-        x = fib(i)
-    print("fibonacci number is :")
-    print(x)
+def main():
+    print("Enter the number :")
+    t = int(input())
+    if(t == 0):
+        print("Enter correct number!")
+    else:
+            
+        for i in range(t):
+            x = fib(i)
+        print("fibonacci number is :")
+        print(x)
+
+if __name__ == '__main__':
+    main()
