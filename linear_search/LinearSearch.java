ba12819679cfcfaf0f7775a25f25995ef0f1cb4a
Sanvi <107338001+sanvi-05@users.noreply.github.com>
2023-10-02 11:31:54 +0530

    Update LinearSearch.java
    Input validation checks for a null input array, and if the input array is null, it throws an IllegalArgumentException with an error message indicating that the input array cannot be null. This ensures that the program handles invalid input cases where the input array is null.

--- linear_search/LinearSearch.java
+++ linear_search/LinearSearch.java
@@ -5,6 +5,10 @@
 public class LinearSearch {
 
     public static int linearSearch(int[] arr, int searchElement) {
+        if (arr == null) {
+            throw new IllegalArgumentException("Input array cannot be null.");
+        }
+        
         for(int i = 0; i < arr.length; i++) {
             if(arr[i] == searchElement)
                 return i;	// Element found
@@ -19,12 +23,16 @@ public class LinearSearch {
             array[i-1] = i;
         // Elements to be searched in the array
         int[] search = {12,55,34,102,78};
-        for(int i = 0; i < 5; i++) {
-            int index = linearSearch(array, search[i]);
-            if(index >= 0)
-                System.out.println(search[i] + " found at index " + index);
-            else
-                System.out.println(search[i] + " not found in the list");
+        for (int i = 0; i < 5; i++) {
+            try {
+                int index = linearSearch(array, search[i]);
+                if (index >= 0)
+                    System.out.println(search[i] + " found at index " + index);
+                else
+                    System.out.println(search[i] + " not found in the list");
+            } catch (IllegalArgumentException e) {
+                System.out.println("Invalid input: " + e.getMessage());
+            }
         }
     }
 }
