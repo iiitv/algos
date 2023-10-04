1976a8a41f2fbdf1b71a93a469b35a776ab7e474
Sourabh Patil <120791252+Acepatil@users.noreply.github.com>
2023-10-04 08:15:07 +0530

    Added space after if statement on line 8 bubble_sort.py

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -5,7 +5,7 @@ def bubble_sort(arr):
     """
     for index_1 in range(len(arr)):
         for index_2 in range(0,len(arr)-index_1-1):
-            if(arr[index_2]>arr[index_2+1]):
+            if (arr[index_2]>arr[index_2+1]):
                 temp=arr[index_2+1]
                 arr[index_2+1]=arr[index_2]
                 arr[index_2]=temp
